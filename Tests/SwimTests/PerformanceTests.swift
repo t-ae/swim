c8ece0f0f4428881781c2d152941e5ae5267b200
araki <araki@qoncept.co.jp>
2017-08-30 18:26:48 +0900

    Hide performance tests

--- Tests/SwimTests/PerformanceTests.swift
+++ Tests/SwimTests/PerformanceTests.swift
@@ -3,7 +3,7 @@ import XCTest
 import Swim
 
 class PerformanceTests: XCTestCase {
-    
+    #if !SWIFT_PACKAGE
     func testConvertedPerformance() {
         let data = [UInt8](repeating: 0, count: 1920*1024*4)
         let image = Image<RGBA, UInt8>(width: 1920, height: 1024, data: data)
@@ -32,4 +32,5 @@ class PerformanceTests: XCTestCase {
             _ = image.toBrightness()
         }
     }
+    #endif
 }
