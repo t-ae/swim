2ce8ff17e3dd671af6a43a3294a56ab99c0db2ae
araki <araki@qoncept.co.jp>
2017-08-31 12:18:33 +0900

    Refactor

--- Tests/SwimTests/ImageSliceTests.swift
+++ Tests/SwimTests/ImageSliceTests.swift
@@ -2,64 +2,64 @@
 import XCTest
 import Swim
 
-class ImageSliceTests: XCTestCase {
+class SubimageSubscriptTests: XCTestCase {
     
-    func testSliceSubscriptGet() {
+    func testSubimageSubscriptGet() {
         let data = (0..<400).map { Double($0) }
         let image = Image<RGBA, Double>(width: 10, height: 10, data: data)
         
         do {
-            let slice = image[0..<2, 0..<3]
+            let subimage = image[0..<2, 0..<3]
             
-            XCTAssertEqual(slice.data, [0, 1, 2, 3,
-                                         4, 5, 6, 7,
-                                         40, 41, 42, 43,
-                                         44, 45, 46, 47,
-                                         80, 81, 82, 83,
-                                         84, 85, 86, 87])
+            XCTAssertEqual(subimage.data, [0, 1, 2, 3,
+                                           4, 5, 6, 7,
+                                           40, 41, 42, 43,
+                                           44, 45, 46, 47,
+                                           80, 81, 82, 83,
+                                           84, 85, 86, 87])
         }
         
         do {
-            let slice = image[1..<4, 0..<2]
+            let subimage = image[1..<4, 0..<2]
             
-            XCTAssertEqual(slice.data, [4, 5, 6, 7,
-                                         8, 9, 10, 11,
-                                         12, 13, 14, 15,
-                                         44, 45, 46, 47,
-                                         48, 49, 50, 51,
-                                         52, 53, 54, 55])
+            XCTAssertEqual(subimage.data, [4, 5, 6, 7,
+                                           8, 9, 10, 11,
+                                           12, 13, 14, 15,
+                                           44, 45, 46, 47,
+                                           48, 49, 50, 51,
+                                           52, 53, 54, 55])
         }
         
         do {
-            let slice = image[1..<3, 1..<3]
+            let subimage = image[1..<3, 1..<3]
             
-            XCTAssertEqual(slice.data, [44, 45, 46, 47,
-                                         48, 49, 50, 51,
-                                         84, 85, 86, 87,
-                                         88, 89, 90, 91])
+            XCTAssertEqual(subimage.data, [44, 45, 46, 47,
+                                           48, 49, 50, 51,
+                                           84, 85, 86, 87,
+                                           88, 89, 90, 91])
         }
         
         do {
-            let slice = image[rows: 1..<2]
-            XCTAssertEqual(slice.data, (40..<80).map(Double.init))
+            let subimage = image[rows: 1..<2]
+            XCTAssertEqual(subimage.data, (40..<80).map(Double.init))
         }
         
         do {
-            let slice = image[cols: 1..<2]
-            XCTAssertEqual(slice.data, [4, 5, 6, 7,
-                                         44, 45, 46, 47,
-                                         84, 85, 86, 87,
-                                         124, 125, 126, 127,
-                                         164, 165, 166, 167,
-                                         204, 205, 206, 207,
-                                         244, 245, 246, 247,
-                                         284, 285, 286, 287,
-                                         324, 325, 326, 327,
-                                         364, 365, 366, 367])
+            let subimage = image[cols: 1..<2]
+            XCTAssertEqual(subimage.data, [4, 5, 6, 7,
+                                           44, 45, 46, 47,
+                                           84, 85, 86, 87,
+                                           124, 125, 126, 127,
+                                           164, 165, 166, 167,
+                                           204, 205, 206, 207,
+                                           244, 245, 246, 247,
+                                           284, 285, 286, 287,
+                                           324, 325, 326, 327,
+                                           364, 365, 366, 367])
         }
     }
     
-    func testSliceSubscriptSet() {
+    func testSubimageSubscriptSet() {
         let data = (0..<3*3*3).map { Double($0) }
         var image = Image<RGB, Double>(width: 3, height: 3, data: data)
         
--- Tests/SwimTests/PerformanceTests.swift
+++ Tests/SwimTests/PerformanceTests.swift
@@ -5,8 +5,8 @@ import Swim
 class PerformanceTests: XCTestCase {
     #if !SWIFT_PACKAGE
     func testConvertedPerformance() {
-        let data = [UInt8](repeating: 0, count: 1920*1024*4)
-        let image = Image<RGBA, UInt8>(width: 1920, height: 1024, data: data)
+        let data = [UInt8](repeating: 0, count: 1920*1080*4)
+        let image = Image<RGBA, UInt8>(width: 1920, height: 1080, data: data)
         
         measure {
             _ = image.converted { x, y, px in
@@ -16,8 +16,8 @@ class PerformanceTests: XCTestCase {
     }
     
     func testChannelwiseConverted() {
-        let data = [UInt8](repeating: 0, count: 1920*1024*4)
-        let image = Image<RGBA, UInt8>(width: 1920, height: 1024, data: data)
+        let data = [UInt8](repeating: 0, count: 1920*1080*4)
+        let image = Image<RGBA, UInt8>(width: 1920, height: 1080, data: data)
         
         measure {
             _ = image.channelwiseConverted { $0 }
@@ -25,11 +25,33 @@ class PerformanceTests: XCTestCase {
     }
     
     func testBrightness() {
-        let data = [Float](repeating: 0, count: 1920*1024*3)
-        let image = Image<RGB, Float>(width: 1920, height: 1024, data: data)
+        let data = [Float](repeating: 0, count: 1920*1080*3)
+        let image = Image<RGB, Float>(width: 1920, height: 1080, data: data)
         
         measure {
             _ = image.toBrightness()
+        }
+    }
+    
+    func testSubimageSubscript() {
+        let data = [Float](repeating: 0, count: 1920*1080*3)
+        let image = Image<RGB, Float>(width: 1920, height: 1080, data: data)
+        
+        measure {
+            for _ in 0..<100_000 {
+                _ = image[0..<16, 0..<16]
+            }
+        }
+    }
+    
+    func testChannelSubscript() {
+        let data = [Float](repeating: 0, count: 1920*1080*3)
+        let image = Image<RGB, Float>(width: 1920, height: 1080, data: data)
+        
+        measure {
+            for _ in 0..<1 {
+                _ = image[channel: .red]
+            }
         }
     }
     #endif
