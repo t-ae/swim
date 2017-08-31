3bdd4e7422ec6826120122caaf84f7ba1851ad02
araki <araki@qoncept.co.jp>
2017-08-31 17:41:36 +0900

    Add FloatingPoint

---
+++ Sources/Swim/Manipulation/FloatingPoint.swift
@@ -0,0 +1,102 @@
+
+import Foundation
+
+extension Image where T: BinaryFloatingPoint {
+    mutating func _round() {
+        channelwiseConvert(Foundation.round)
+    }
+    
+    public mutating func round() {
+        _round()
+    }
+    
+    public func rounded() -> Image<P, T> {
+        var newImage = self
+        newImage._round()
+        return newImage
+    }
+    
+    mutating func _ceil() {
+        channelwiseConvert(Foundation.ceil)
+    }
+    
+    public mutating func ceil() {
+        _ceil()
+    }
+    
+    public func ceiled() -> Image<P, T> {
+        var newImage = self
+        newImage._ceil()
+        return newImage
+    }
+    
+    mutating func _floor() {
+        channelwiseConvert(Foundation.floor)
+    }
+    
+    public mutating func floor() {
+        _floor()
+    }
+    
+    public func floored() -> Image<P, T> {
+        var newImage = self
+        newImage._floor()
+        return newImage
+    }
+}
+
+#if os(macOS) || os(iOS)
+    import Accelerate
+    
+    extension Image where T == Float {
+        mutating func _round() {
+            unsafeChannelwiseConvert {
+                let p = $0.baseAddress!
+                var count = Int32($0.count)
+                vvnintf(p, p, &count)
+            }
+        }
+        
+        mutating func _ceil() {
+            unsafeChannelwiseConvert {
+                let p = $0.baseAddress!
+                var count = Int32($0.count)
+                vvceilf(p, p, &count)
+            }
+        }
+        
+        mutating func _floor() {
+            unsafeChannelwiseConvert {
+                let p = $0.baseAddress!
+                var count = Int32($0.count)
+                vvfloorf(p, p, &count)
+            }
+        }
+    }
+    
+    extension Image where T == Double {
+        mutating func _round() {
+            unsafeChannelwiseConvert {
+                let p = $0.baseAddress!
+                var count = Int32($0.count)
+                vvnint(p, p, &count)
+            }
+        }
+        
+        mutating func _ceil() {
+            unsafeChannelwiseConvert {
+                let p = $0.baseAddress!
+                var count = Int32($0.count)
+                vvceil(p, p, &count)
+            }
+        }
+        
+        mutating func _floor() {
+            unsafeChannelwiseConvert {
+                let p = $0.baseAddress!
+                var count = Int32($0.count)
+                vvfloor(p, p, &count)
+            }
+        }
+    }
+#endif
---
+++ Tests/SwimTests/FloatingPointTests.swift
@@ -0,0 +1,69 @@
+
+import XCTest
+import Swim
+
+class FloatingPointTests: XCTestCase {
+    
+    func testRound() {
+        do {
+            let image = Image<RGBA, Float>(width: 2, height: 2, data: (1...16).map(Float.init))
+            
+            XCTAssertEqual((image + 0.1).rounded(), image)
+            
+            var image2 = image + 0.1
+            image2.round()
+            XCTAssertEqual(image2, image)
+        }
+        do {
+            let image = Image<RGBA, Double>(width: 2, height: 2, data: (1...16).map(Double.init))
+            
+            XCTAssertEqual((image + 0.1).rounded(), image)
+            
+            var image2 = image + 0.1
+            image2.round()
+            XCTAssertEqual(image2, image)
+        }
+    }
+    
+    func testCeil() {
+        do {
+            let image = Image<RGBA, Float>(width: 2, height: 2, data: (1...16).map(Float.init))
+            
+            XCTAssertEqual((image - 0.1).ceiled(), image)
+            
+            var image2 = image - 0.1
+            image2.ceil()
+            XCTAssertEqual(image2, image)
+        }
+        do {
+            let image = Image<RGBA, Double>(width: 2, height: 2, data: (1...16).map(Double.init))
+            
+            XCTAssertEqual((image - 0.1).ceiled(), image)
+            
+            var image2 = image - 0.1
+            image2.ceil()
+            XCTAssertEqual(image2, image)
+        }
+    }
+    
+    func testFloor() {
+        do {
+            let image = Image<RGBA, Float>(width: 2, height: 2, data: (1...16).map(Float.init))
+            
+            XCTAssertEqual((image + 0.1).floored(), image)
+            
+            var image2 = image + 0.1
+            image2.floor()
+            XCTAssertEqual(image2, image)
+        }
+        do {
+            let image = Image<RGBA, Double>(width: 2, height: 2, data: (1...16).map(Double.init))
+            
+            XCTAssertEqual((image + 0.1).floored(), image)
+            
+            var image2 = image + 0.1
+            image2.floor()
+            XCTAssertEqual(image2, image)
+        }
+    }
+}
