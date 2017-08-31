10a68cd526129f9a284ae8c4f533e3f581112209
araki <araki@qoncept.co.jp>
2017-08-31 16:52:42 +0900

    Add test

--- Tests/SwimTests/ImageTests.swift
+++ Tests/SwimTests/ImageTests.swift
@@ -22,6 +22,26 @@ class ImageTests: XCTestCase {
             
             XCTAssertEqual(image.data, data)
         }
-        
+    }
+    
+    func testEquality() {
+        do {
+            let image1 = Image<Intensity, Bool>(width: 2, height: 2, data: [true, false, false, true])
+            let image2 = Image<Intensity, Bool>(width: 2, height: 2, data: [true, false, true, true])
+            XCTAssertTrue(image1 == image1)
+            XCTAssertFalse(image1 == image2)
+        }
+        do {
+            let data = (0..<100).map { UInt8($0) }
+            let image = Image<Intensity, UInt8>(width: 10, height: 10, data: data)
+            
+            XCTAssertTrue(image == image)
+        }
+        do {
+            let data = (0..<400).map { Double($0) }
+            let image = Image<RGBA, Double>(width: 10, height: 10, data: data)
+            
+            XCTAssertTrue(image == image)
+        }
     }
 }
