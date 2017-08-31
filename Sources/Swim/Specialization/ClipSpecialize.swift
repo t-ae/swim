b3fec3956082a17e890c11f118066ce1386cbfe2
araki <araki@qoncept.co.jp>
2017-08-31 17:21:21 +0900

    Fix: ClipSpecialize

---
+++ Sources/Swim/Specialization/ClipSpecialize.swift
@@ -0,0 +1,193 @@
+
+extension Image where P == Intensity, T == UInt8 {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == Intensity, T == Int {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == Intensity, T == Float {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == Intensity, T == Double {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGB, T == UInt8 {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGB, T == Int {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGB, T == Float {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGB, T == Double {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGBA, T == UInt8 {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGBA, T == Int {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGBA, T == Float {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == RGBA, T == Double {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == ARGB, T == UInt8 {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == ARGB, T == Int {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == ARGB, T == Float {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
+extension Image where P == ARGB, T == Double {
+    public mutating func clip(low: T, high: T) {
+        _clip(low: low, high: high)
+    }
+    
+    public func clipped(low: T, high: T) -> Image<P, T> {
+        var ret = self
+        ret._clip(low: low, high: high)
+        return ret
+    }
+}
+
