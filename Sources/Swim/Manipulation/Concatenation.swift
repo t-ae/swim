c86d3531f1c10f0b25b5a4be0a63c6bc6b82d4d4
araki <araki@qoncept.co.jp>
2017-09-29 18:43:17 +0900

    Add ConcatenationSpecialize

--- Sources/Swim/Manipulation/Concatenation.swift
+++ Sources/Swim/Manipulation/Concatenation.swift
@@ -1,5 +1,5 @@
 
-public func concatH<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
+func _concatH<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
     let width = images.map { $0.width }.reduce(0, +)
     let height = images[0].height
     precondition(!images.contains { $0.height != height })
@@ -15,7 +15,11 @@ public func concatH<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
     return image
 }
 
-public func concatV<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
+public func concatH<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
+    return _concatH(images)
+}
+
+func _concatV<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
     let width = images[0].width
     let height = images.map { $0.height }.reduce(0, +)
     precondition(!images.contains { $0.width != width })
@@ -29,4 +33,8 @@ public func concatV<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
     }
     
     return image
+}
+
+public func concatV<P, T>(_ images: [Image<P, T>]) -> Image<P, T> {
+    return _concatV(images)
 }
---
+++ Sources/Swim/Specialization/ConcatenationSpecialize.swift
@@ -0,0 +1,201 @@
+
+public func concatH(_ images: [Image<Intensity, Bool>]) -> Image<Intensity, Bool> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<Intensity, Bool>]) -> Image<Intensity, Bool> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<Intensity, UInt8>]) -> Image<Intensity, UInt8> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<Intensity, UInt8>]) -> Image<Intensity, UInt8> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<Intensity, Int>]) -> Image<Intensity, Int> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<Intensity, Int>]) -> Image<Intensity, Int> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<Intensity, Float>]) -> Image<Intensity, Float> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<Intensity, Float>]) -> Image<Intensity, Float> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<Intensity, Double>]) -> Image<Intensity, Double> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<Intensity, Double>]) -> Image<Intensity, Double> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<IntensityAlpha, Bool>]) -> Image<IntensityAlpha, Bool> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<IntensityAlpha, Bool>]) -> Image<IntensityAlpha, Bool> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<IntensityAlpha, UInt8>]) -> Image<IntensityAlpha, UInt8> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<IntensityAlpha, UInt8>]) -> Image<IntensityAlpha, UInt8> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<IntensityAlpha, Int>]) -> Image<IntensityAlpha, Int> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<IntensityAlpha, Int>]) -> Image<IntensityAlpha, Int> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<IntensityAlpha, Float>]) -> Image<IntensityAlpha, Float> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<IntensityAlpha, Float>]) -> Image<IntensityAlpha, Float> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<IntensityAlpha, Double>]) -> Image<IntensityAlpha, Double> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<IntensityAlpha, Double>]) -> Image<IntensityAlpha, Double> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGB, Bool>]) -> Image<RGB, Bool> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGB, Bool>]) -> Image<RGB, Bool> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGB, UInt8>]) -> Image<RGB, UInt8> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGB, UInt8>]) -> Image<RGB, UInt8> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGB, Int>]) -> Image<RGB, Int> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGB, Int>]) -> Image<RGB, Int> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGB, Float>]) -> Image<RGB, Float> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGB, Float>]) -> Image<RGB, Float> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGB, Double>]) -> Image<RGB, Double> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGB, Double>]) -> Image<RGB, Double> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGBA, Bool>]) -> Image<RGBA, Bool> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGBA, Bool>]) -> Image<RGBA, Bool> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGBA, UInt8>]) -> Image<RGBA, UInt8> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGBA, UInt8>]) -> Image<RGBA, UInt8> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGBA, Int>]) -> Image<RGBA, Int> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGBA, Int>]) -> Image<RGBA, Int> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGBA, Float>]) -> Image<RGBA, Float> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGBA, Float>]) -> Image<RGBA, Float> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<RGBA, Double>]) -> Image<RGBA, Double> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<RGBA, Double>]) -> Image<RGBA, Double> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<ARGB, Bool>]) -> Image<ARGB, Bool> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<ARGB, Bool>]) -> Image<ARGB, Bool> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<ARGB, UInt8>]) -> Image<ARGB, UInt8> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<ARGB, UInt8>]) -> Image<ARGB, UInt8> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<ARGB, Int>]) -> Image<ARGB, Int> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<ARGB, Int>]) -> Image<ARGB, Int> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<ARGB, Float>]) -> Image<ARGB, Float> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<ARGB, Float>]) -> Image<ARGB, Float> {
+    return _concatV(images)
+}
+
+public func concatH(_ images: [Image<ARGB, Double>]) -> Image<ARGB, Double> {
+    return _concatH(images)
+}
+
+public func concatV(_ images: [Image<ARGB, Double>]) -> Image<ARGB, Double> {
+    return _concatV(images)
+}
+
