ae043bef63a8896bb23b3e977e004424451cd704
araki <araki@qoncept.co.jp>
2017-09-29 15:23:24 +0900

    Refine Arithmetic

--- Sources/Swim/Manipulation/Arithmetic.swift
+++ Sources/Swim/Manipulation/Arithmetic.swift
@@ -1,18 +1,7 @@
 
 // protocol
-public protocol CompoundArithmetics {
-    static func +=(lhs: inout Self, rhs: Self)
-    static func -=(lhs: inout Self, rhs: Self)
-    static func *=(lhs: inout Self, rhs: Self)
-    static func /=(lhs: inout Self, rhs: Self)
-}
 
-extension UInt8: CompoundArithmetics {}
-extension Int: CompoundArithmetics {}
-extension Float: CompoundArithmetics {}
-extension Double: CompoundArithmetics {}
-
-extension DataContainer where DT: CompoundArithmetics {
+extension DataContainer where DT: Numeric {
     
     static func add(lhs: inout Self, rhs: DT) {
         lhs.data.withUnsafeMutableBufferPointer {
@@ -39,16 +28,6 @@ extension DataContainer where DT: CompoundArithmetics {
             var p = $0.baseAddress!
             for _ in 0..<$0.count {
                 p.pointee *= rhs
-                p += 1
-            }
-        }
-    }
-    
-    static func divide(lhs: inout Self, rhs: DT) {
-        lhs.data.withUnsafeMutableBufferPointer {
-            var p = $0.baseAddress!
-            for _ in 0..<$0.count {
-                p.pointee /= rhs
                 p += 1
             }
         }
@@ -83,6 +62,40 @@ extension DataContainer where DT: CompoundArithmetics {
     public static func *=(lhs: inout Self, rhs: DT) {
         multiply(lhs: &lhs, rhs: rhs)
     }
+}
+
+extension DataContainer where DT: BinaryInteger {
+    static func divide(lhs: inout Self, rhs: DT) {
+        lhs.data.withUnsafeMutableBufferPointer {
+            var p = $0.baseAddress!
+            for _ in 0..<$0.count {
+                p.pointee /= rhs
+                p += 1
+            }
+        }
+    }
+    
+    public static func /(lhs: Self, rhs: DT) -> Self {
+        var ret = lhs
+        ret /= rhs
+        return ret
+    }
+    
+    public static func /=(lhs: inout Self, rhs: DT) {
+        divide(lhs: &lhs, rhs: rhs)
+    }
+}
+
+extension DataContainer where DT: BinaryFloatingPoint {
+    static func divide(lhs: inout Self, rhs: DT) {
+        lhs.data.withUnsafeMutableBufferPointer {
+            var p = $0.baseAddress!
+            for _ in 0..<$0.count {
+                p.pointee /= rhs
+                p += 1
+            }
+        }
+    }
     
     public static func /(lhs: Self, rhs: DT) -> Self {
         var ret = lhs
@@ -100,26 +113,26 @@ extension DataContainer where DT: CompoundArithmetics {
 #if os(macOS) || os(iOS)
     import Accelerate
     
-    extension Image where T == Float {
-        static func add(lhs: inout Image<P, T>, rhs: T) {
+    extension DataContainer where DT == Float {
+        static func add(lhs: inout Image<PT, DT>, rhs: DT) {
             var rhs = rhs
             lhs._data.withUnsafeMutableBufferPointer {
                 vDSP_vsadd($0.baseAddress!, 1, &rhs, $0.baseAddress!, 1, vDSP_Length($0.count))
             }
         }
         
-        static func subtract(lhs: inout Image<P, T>, rhs: T) {
+        static func subtract(lhs: inout Image<PT, DT>, rhs: DT) {
             add(lhs: &lhs, rhs: -rhs)
         }
         
-        static func multiply(lhs: inout Image<P, T>, rhs: T) {
+        static func multiply(lhs: inout Image<PT, DT>, rhs: DT) {
             var rhs = rhs
             lhs._data.withUnsafeMutableBufferPointer {
                 vDSP_vsmul($0.baseAddress!, 1, &rhs, $0.baseAddress!, 1, vDSP_Length($0.count))
             }
         }
         
-        static func divide(lhs: inout Image<P, T>, rhs: T) {
+        static func divide(lhs: inout Image<PT, DT>, rhs: DT) {
             var rhs = rhs
             lhs._data.withUnsafeMutableBufferPointer {
                 vDSP_vsdiv($0.baseAddress!, 1, &rhs, $0.baseAddress!, 1, vDSP_Length($0.count))
@@ -127,26 +140,26 @@ extension DataContainer where DT: CompoundArithmetics {
         }
     }
     
-    extension Image where T == Double {
-        static func add(lhs: inout Image<P, T>, rhs: T) {
+    extension DataContainer where DT == Double {
+        static func add(lhs: inout Image<PT, DT>, rhs: DT) {
             var rhs = rhs
             lhs._data.withUnsafeMutableBufferPointer {
                 vDSP_vsaddD($0.baseAddress!, 1, &rhs, $0.baseAddress!, 1, vDSP_Length($0.count))
             }
         }
         
-        static func subtract(lhs: inout Image<P, T>, rhs: T) {
+        static func subtract(lhs: inout Image<PT, DT>, rhs: DT) {
             add(lhs: &lhs, rhs: -rhs)
         }
         
-        static func multiply(lhs: inout Image<P, T>, rhs: T) {
+        static func multiply(lhs: inout Image<PT, DT>, rhs: DT) {
             var rhs = rhs
             lhs._data.withUnsafeMutableBufferPointer {
                 vDSP_vsmulD($0.baseAddress!, 1, &rhs, $0.baseAddress!, 1, vDSP_Length($0.count))
             }
         }
         
-        static func divide(lhs: inout Image<P, T>, rhs: T) {
+        static func divide(lhs: inout Image<PT, DT>, rhs: DT) {
             var rhs = rhs
             lhs._data.withUnsafeMutableBufferPointer {
                 vDSP_vsdivD($0.baseAddress!, 1, &rhs, $0.baseAddress!, 1, vDSP_Length($0.count))
