15854d2adeafc484bfab56fee0dd7b4ea0ba5c8c
araki <araki@qoncept.co.jp>
2017-09-29 15:52:48 +0900

    Add specialization

--- Sources/Swim/Specialization/PixelTypeConversionSpecialize.swift
+++ Sources/Swim/Specialization/PixelTypeConversionSpecialize.swift
@@ -160,3 +160,74 @@ extension Image where P == RGB, T == Double {
 
 }
 
+
+extension Image where P == RGBA, T == Bool {
+    public init(image: Image<ARGB, T>) {
+        let data = permuteChannels(data: image.data, permutation: [1, 2, 3, 0])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == ARGB, T == Bool  {
+    public init(image: Image<RGBA, T>) {
+        let data = permuteChannels(data: image.data, permutation: [3, 0, 1, 2])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == RGBA, T == UInt8 {
+    public init(image: Image<ARGB, T>) {
+        let data = permuteChannels(data: image.data, permutation: [1, 2, 3, 0])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == ARGB, T == UInt8  {
+    public init(image: Image<RGBA, T>) {
+        let data = permuteChannels(data: image.data, permutation: [3, 0, 1, 2])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == RGBA, T == Int {
+    public init(image: Image<ARGB, T>) {
+        let data = permuteChannels(data: image.data, permutation: [1, 2, 3, 0])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == ARGB, T == Int  {
+    public init(image: Image<RGBA, T>) {
+        let data = permuteChannels(data: image.data, permutation: [3, 0, 1, 2])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == RGBA, T == Float {
+    public init(image: Image<ARGB, T>) {
+        let data = permuteChannels(data: image.data, permutation: [1, 2, 3, 0])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == ARGB, T == Float  {
+    public init(image: Image<RGBA, T>) {
+        let data = permuteChannels(data: image.data, permutation: [3, 0, 1, 2])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == RGBA, T == Double {
+    public init(image: Image<ARGB, T>) {
+        let data = permuteChannels(data: image.data, permutation: [1, 2, 3, 0])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
+extension Image where P == ARGB, T == Double  {
+    public init(image: Image<RGBA, T>) {
+        let data = permuteChannels(data: image.data, permutation: [3, 0, 1, 2])
+        self.init(width: image.width, height: image.height, data: data)
+    }
+}
+
