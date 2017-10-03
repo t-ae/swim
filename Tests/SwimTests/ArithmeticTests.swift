20b4cb2afb6bbe459b0402e4743df28e8a47bbbe
araki <araki@qoncept.co.jp>
2017-10-03 15:32:50 +0900

    Improve tests

--- Tests/SwimTests/ArithmeticTests.swift
+++ Tests/SwimTests/ArithmeticTests.swift
@@ -17,6 +17,12 @@ class ArithmeticTests: XCTestCase {
             let image = Image<RGBA, Float>(width: 2, height: 2, data: before)
             XCTAssertEqual(-image, Image(width: 2, height: 2, data: after))
         }
+        do {
+            let before = (0..<16).map { Double.init($0) }
+            let after = before.map { -$0 }
+            let image = Image<ARGB, Double>(width: 2, height: 2, data: before)
+            XCTAssertEqual(-image, Image(width: 2, height: 2, data: after))
+        }
     }
     
     func testAdd() {
@@ -33,6 +39,15 @@ class ArithmeticTests: XCTestCase {
             let before = (0..<16).map { Float.init($0) }
             let after = before.map { $0 + 1 }
             var image = Image<RGBA, Float>(width: 2, height: 2, data: before)
+            XCTAssertEqual(image + 1, Image(width: 2, height: 2, data: after))
+            XCTAssertEqual(1 + image, Image(width: 2, height: 2, data: after))
+            image += 1
+            XCTAssertEqual(image, Image(width: 2, height: 2, data: after))
+        }
+        do {
+            let before = (0..<16).map { Double.init($0) }
+            let after = before.map { $0 + 1 }
+            var image = Image<ARGB, Double>(width: 2, height: 2, data: before)
             XCTAssertEqual(image + 1, Image(width: 2, height: 2, data: after))
             XCTAssertEqual(1 + image, Image(width: 2, height: 2, data: after))
             image += 1
@@ -61,6 +76,16 @@ class ArithmeticTests: XCTestCase {
             image -= 1
             XCTAssertEqual(image, Image(width: 2, height: 2, data: after1))
         }
+        do {
+            let before = (0..<16).map { Double.init($0) }
+            var image = Image<ARGB, Double>(width: 2, height: 2, data: before)
+            let after1 = before.map { $0 - 1 }
+            XCTAssertEqual(image - 1, Image(width: 2, height: 2, data: after1))
+            let after2 = before.map { 255 - $0 }
+            XCTAssertEqual(255 - image, Image(width: 2, height: 2, data: after2))
+            image -= 1
+            XCTAssertEqual(image, Image(width: 2, height: 2, data: after1))
+        }
     }
     
     func testMul() {
@@ -82,6 +107,15 @@ class ArithmeticTests: XCTestCase {
             image *= 2
             XCTAssertEqual(image, Image(width: 2, height: 2, data: after))
         }
+        do {
+            let before = (0..<16).map { Double.init($0) }
+            let after = before.map { $0 * 2 }
+            var image = Image<ARGB, Double>(width: 2, height: 2, data: before)
+            XCTAssertEqual(image * 2, Image(width: 2, height: 2, data: after))
+            XCTAssertEqual(2 * image, Image(width: 2, height: 2, data: after))
+            image *= 2
+            XCTAssertEqual(image, Image(width: 2, height: 2, data: after))
+        }
     }
     
     func testDiv() {
@@ -98,6 +132,16 @@ class ArithmeticTests: XCTestCase {
         do {
             let before = (0..<16).map { Float.init($0+1) }
             var image = Image<RGBA, Float>(width: 2, height: 2, data: before)
+            let after1 = before.map { $0 / 2 }
+            XCTAssertEqual(image / 2, Image(width: 2, height: 2, data: after1))
+            let after2 = before.map { 10 / $0 }
+            XCTAssertEqual(10 / image, Image(width: 2, height: 2, data: after2), accuracy: 1e-3)
+            image /= 2
+            XCTAssertEqual(image, Image(width: 2, height: 2, data: after1))
+        }
+        do {
+            let before = (0..<16).map { Double.init($0+1) }
+            var image = Image<ARGB, Double>(width: 2, height: 2, data: before)
             let after1 = before.map { $0 / 2 }
             XCTAssertEqual(image / 2, Image(width: 2, height: 2, data: after1))
             let after2 = before.map { 10 / $0 }
--- Tests/SwimTests/ChannelCompositionTests.swift
+++ Tests/SwimTests/ChannelCompositionTests.swift
@@ -29,7 +29,18 @@ class ChannelCompositionTests: XCTestCase {
             XCTAssertEqual(image[channel: .blue], blue)
             XCTAssertEqual(image[channel: .alpha], alpha)
         }
-        
+        do {
+            let red = Image<Intensity, Double>(width: 2, height: 2, data: [0, 1, 2, 3])
+            let green = Image<Intensity, Double>(width: 2, height: 2, data: [4, 5, 6, 7])
+            let blue = Image<Intensity, Double>(width: 2, height: 2, data: [8, 9, 10, 11])
+            let alpha = Image<Intensity, Double>(width: 2, height: 2, data: [12, 13, 14, 15])
+            
+            let image = Image<ARGB, Double>(a: alpha, r: red, g: green, b: blue)
+            
+            XCTAssertEqual(image[channel: .red], red)
+            XCTAssertEqual(image[channel: .green], green)
+            XCTAssertEqual(image[channel: .blue], blue)
+            XCTAssertEqual(image[channel: .alpha], alpha)
+        }
     }
-    
 }
