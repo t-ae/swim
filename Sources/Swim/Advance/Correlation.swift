4673ff5e12ba0763ca65d68581ef1a02fcaf684d
araki <araki@qoncept.co.jp>
2017-10-31 12:41:29 +0900

    Change correlation interface

--- Sources/Swim/Advance/Correlation.swift
+++ Sources/Swim/Advance/Correlation.swift
@@ -1,121 +1,117 @@
 
 import Foundation
 
-func _calculateSSD<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                           _ b: Image<Intensity, T>) -> T {
-    precondition(a.size == b.size, "Images must have same size.")
-    
-    var sum: T = 0
-    a.data.withUnsafeBufferPointer {
-        var ap = $0.baseAddress!
-        b.data.withUnsafeBufferPointer {
-            var bp = $0.baseAddress!
-            
-            for _ in 0..<$0.count {
-                sum += (ap.pointee - bp.pointee) * (ap.pointee - bp.pointee)
-                ap += 1
-                bp += 1
+public enum Correlation<T: BinaryFloatingPoint&DataType> {
+    static func _ssd(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        precondition(a.size == b.size, "Images must have same size.")
+        
+        var sum: T = 0
+        a.data.withUnsafeBufferPointer {
+            var ap = $0.baseAddress!
+            b.data.withUnsafeBufferPointer {
+                var bp = $0.baseAddress!
+                
+                for _ in 0..<$0.count {
+                    sum += (ap.pointee - bp.pointee) * (ap.pointee - bp.pointee)
+                    ap += 1
+                    bp += 1
+                }
             }
         }
+        return sum
     }
-    return sum
-}
-
-public func calculateSSD<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                                 _ b: Image<Intensity, T>) -> T {
-    return _calculateSSD(a, b)
-}
-
-func _calculateSAD<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                           _ b: Image<Intensity, T>) -> T {
-    precondition(a.size == b.size, "Images must have same size.")
     
-    var sum: T = 0
-    a.data.withUnsafeBufferPointer {
-        var ap = $0.baseAddress!
-        b.data.withUnsafeBufferPointer {
-            var bp = $0.baseAddress!
-            
-            for _ in 0..<$0.count {
-                sum += abs(ap.pointee - bp.pointee)
-                ap += 1
-                bp += 1
+    public static func ssd(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _ssd(a, b)
+    }
+    
+    static func _sad(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        precondition(a.size == b.size, "Images must have same size.")
+        
+        var sum: T = 0
+        a.data.withUnsafeBufferPointer {
+            var ap = $0.baseAddress!
+            b.data.withUnsafeBufferPointer {
+                var bp = $0.baseAddress!
+                
+                for _ in 0..<$0.count {
+                    sum += abs(ap.pointee - bp.pointee)
+                    ap += 1
+                    bp += 1
+                }
             }
         }
+        return sum
     }
-    return sum
-}
-
-public func calculateSAD<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                                 _ b: Image<Intensity, T>) -> T {
-    return _calculateSAD(a, b)
-}
-
-func _calculateNCC<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                           _ b: Image<Intensity, T>) -> T {
-    precondition(a.size == b.size, "Images must have same size.")
     
-    var sum2a: T = 0
-    var sum2b: T = 0
-    var sumCross: T = 0
+    public static func sad(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _sad(a, b)
+    }
     
-    a.data.withUnsafeBufferPointer {
-        var ap = $0.baseAddress!
-        b.data.withUnsafeBufferPointer {
-            var bp = $0.baseAddress!
-            
-            for _ in 0..<$0.count {
-                sum2a += ap.pointee*ap.pointee
-                sum2b += bp.pointee*bp.pointee
-                sumCross += ap.pointee*bp.pointee
-                ap += 1
-                bp += 1
+    static func _ncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        precondition(a.size == b.size, "Images must have same size.")
+        
+        var sum2a: T = 0
+        var sum2b: T = 0
+        var sumCross: T = 0
+        
+        a.data.withUnsafeBufferPointer {
+            var ap = $0.baseAddress!
+            b.data.withUnsafeBufferPointer {
+                var bp = $0.baseAddress!
+                
+                for _ in 0..<$0.count {
+                    sum2a += ap.pointee*ap.pointee
+                    sum2b += bp.pointee*bp.pointee
+                    sumCross += ap.pointee*bp.pointee
+                    ap += 1
+                    bp += 1
+                }
             }
         }
+        return sumCross / sqrt(sum2a * sum2b)
     }
-    return sumCross / sqrt(sum2a * sum2b)
-}
-
-public func calculateNCC<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                                 _ b: Image<Intensity, T>) -> T {
-    return _calculateNCC(a, b)
-}
-
-func _calculateZNCC<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                            _ b: Image<Intensity, T>) -> T {
-    precondition(a.size == b.size, "Images must have same size.")
     
-    var suma: T = 0
-    var sumb: T = 0
-    var sum2a: T = 0
-    var sum2b: T = 0
-    var sumCross: T = 0
+    public static func ncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _ncc(a, b)
+    }
     
-    a.data.withUnsafeBufferPointer {
-        var ap = $0.baseAddress!
-        b.data.withUnsafeBufferPointer {
-            var bp = $0.baseAddress!
-            
-            for _ in 0..<$0.count {
-                suma += ap.pointee
-                sumb += bp.pointee
-                sum2a += ap.pointee*ap.pointee
-                sum2b += bp.pointee*bp.pointee
-                sumCross += ap.pointee*bp.pointee
-                ap += 1
-                bp += 1
+    static func _zncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        precondition(a.size == b.size, "Images must have same size.")
+        
+        var suma: T = 0
+        var sumb: T = 0
+        var sum2a: T = 0
+        var sum2b: T = 0
+        var sumCross: T = 0
+        
+        a.data.withUnsafeBufferPointer {
+            var ap = $0.baseAddress!
+            b.data.withUnsafeBufferPointer {
+                var bp = $0.baseAddress!
+                
+                for _ in 0..<$0.count {
+                    suma += ap.pointee
+                    sumb += bp.pointee
+                    sum2a += ap.pointee*ap.pointee
+                    sum2b += bp.pointee*bp.pointee
+                    sumCross += ap.pointee*bp.pointee
+                    ap += 1
+                    bp += 1
+                }
             }
         }
+        
+        let c = T(a.width * a.height)
+        let up = (c*sumCross - suma*sumb)
+        let da = (c*sum2a - suma*suma)
+        let db = (c*sum2b - sumb*sumb)
+        return up / sqrt(da*db)
     }
     
-    let c = T(a.width * a.height)
-    let up = (c*sumCross - suma*sumb)
-    let da = (c*sum2a - suma*suma)
-    let db = (c*sum2b - sumb*sumb)
-    return up / sqrt(da*db)
+    public static func zncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _zncc(a, b)
+    }
 }
 
-public func calculateZNCC<T: BinaryFloatingPoint>(_ a: Image<Intensity, T>,
-                                                  _ b: Image<Intensity, T>) -> T {
-    return _calculateZNCC(a, b)
-}
+
--- Sources/Swim/Specialization/CorrelationSpecialize.swift
+++ Sources/Swim/Specialization/CorrelationSpecialize.swift
@@ -1,41 +1,31 @@
 
-public func calculateSSD(_ a: Image<Intensity, Float>,
-                         _ b: Image<Intensity, Float>) -> Float {
-    return _calculateSSD(a, b)
+extension Correlation where T == Float {
+    public static func ssd(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _ssd(a, b)
+    }
+    public static func sad(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _sad(a, b)
+    }
+    public static func ncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _ncc(a, b)
+    }
+    public static func zncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _zncc(a, b)
+    }
 }
 
-public func calculateSAD(_ a: Image<Intensity, Float>,
-                         _ b: Image<Intensity, Float>) -> Float {
-    return _calculateSAD(a, b)
-}
-
-public func calculateNCC(_ a: Image<Intensity, Float>,
-                         _ b: Image<Intensity, Float>) -> Float {
-    return _calculateNCC(a, b)
-}
-
-public func calculateZNCC(_ a: Image<Intensity, Float>,
-                          _ b: Image<Intensity, Float>) -> Float {
-    return _calculateZNCC(a, b)
-}
-
-public func calculateSSD(_ a: Image<Intensity, Double>,
-                         _ b: Image<Intensity, Double>) -> Double {
-    return _calculateSSD(a, b)
-}
-
-public func calculateSAD(_ a: Image<Intensity, Double>,
-                         _ b: Image<Intensity, Double>) -> Double {
-    return _calculateSAD(a, b)
-}
-
-public func calculateNCC(_ a: Image<Intensity, Double>,
-                         _ b: Image<Intensity, Double>) -> Double {
-    return _calculateNCC(a, b)
-}
-
-public func calculateZNCC(_ a: Image<Intensity, Double>,
-                          _ b: Image<Intensity, Double>) -> Double {
-    return _calculateZNCC(a, b)
+extension Correlation where T == Double {
+    public static func ssd(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _ssd(a, b)
+    }
+    public static func sad(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _sad(a, b)
+    }
+    public static func ncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _ncc(a, b)
+    }
+    public static func zncc(_ a: Image<Intensity, T>, _ b: Image<Intensity, T>) -> T {
+        return _zncc(a, b)
+    }
 }
 
