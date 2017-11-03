bde560c416630df91fab419294694ae77b6350ff
araki <araki@qoncept.co.jp>
2017-11-03 16:00:54 +0900

    Improve Im2col

--- Sources/Swim/Advance/Im2col.swift
+++ Sources/Swim/Advance/Im2col.swift
@@ -1,4 +1,6 @@
 
+import Foundation
+
 public enum Padding<T: DataType> {
     case nearest
     case constant(T)
@@ -9,28 +11,73 @@ extension Image where P == Intensity {
                  patchHeight: Int,
                  padding: Padding<T> = .nearest) -> (m: Int, n: Int, matrix: [T]) {
         
-        var ret: [T] = []
-        ret.reserveCapacity(width*height)
+        let m = patchWidth*patchHeight
+        let n = width*height
+        
+        var ret: [T]
+        switch padding {
+        case .constant(let v):
+            ret = [T](repeating: v, count: m*n)
+        default:
+            ret = [T](repeating: T.swimDefaultValue, count: m*n)
+        }
         
         let padTop = (patchHeight-1) / 2
         let padLeft = (patchWidth-1) / 2
         
-        for dy in 0..<patchHeight {
-            for dx in 0..<patchWidth {
-                for y in 0..<height {
-                    let yy = y + dy - padTop
-                    for x in 0..<width {
-                        let xx = x + dx - padLeft
-                        if 0..<width ~= xx && 0..<height ~= yy {
-                            ret.append(self[xx, yy])
-                        } else {
+        data.withUnsafeBufferPointer {
+            let src = $0.baseAddress!
+            ret.withUnsafeMutableBufferPointer {
+                var dst = $0.baseAddress!
+                
+                for dy in -padTop..<patchHeight-padTop {
+                    for dx in -padLeft..<patchWidth-padLeft {
+                        for y in 0..<height {
+                            var yy = y + dy
+                            if !(0..<height ~= yy) {
+                                switch padding {
+                                case .nearest:
+                                    yy = min(max(yy, 0), height-1)
+                                case .constant:
+                                    dst += width
+                                    continue
+                                }
+                            }
+
+                            // point leftest pixel
+                            let x = min(max(dx, 0), width-1)
+                            var sp = src + (yy * width + x)
+                            
+                            // left padding
+                            let padLeftValue: T
                             switch padding {
                             case .nearest:
-                                let xx = min(max(xx, 0), width-1)
-                                let yy = min(max(yy, 0), height-1)
-                                ret.append(self[xx, yy])
+                                padLeftValue = sp.pointee
                             case .constant(let v):
-                                ret.append(v)
+                                padLeftValue = v
+                            }
+                            for _ in 0..<max(-dx, 0) {
+                                dst.pointee = padLeftValue
+                                dst += 1
+                            }
+                            
+                            // copy
+                            let count = width - abs(dx)
+                            memcpy(dst, sp, count*MemoryLayout<T>.size)
+                            dst += count
+                            
+                            // right padding
+                            let padRightValue: T
+                            switch padding {
+                            case .nearest:
+                                sp += count - 1 // point rightest
+                                padRightValue = sp.pointee
+                            case .constant(let v):
+                                padRightValue = v
+                            }
+                            for _ in 0..<max(dx, 0) {
+                                dst.pointee = padRightValue
+                                dst += 1
                             }
                         }
                     }
@@ -38,7 +85,7 @@ extension Image where P == Intensity {
             }
         }
         
-        return (patchWidth*patchHeight, width*height, ret)
+        return (m, n, ret)
     }
     
     public func im2col(patchWidth: Int,
