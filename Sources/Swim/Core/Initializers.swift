8b46cd7835cf5865c6a43da3a49cf3564edb9202
araki <araki@qoncept.co.jp>
2018-02-02 09:21:25 +0900

    Add initializers

---
+++ Sources/Swim/Core/Initializers.swift
@@ -0,0 +1,30 @@
+
+extension Image where P == Intensity {
+    public init(width: Int, height: Int, intensity: [T]) {
+        self.init(width: width, height: height, data: intensity)
+    }
+}
+
+extension Image where P == IntensityAlpha {
+    public init(width: Int, height: Int, intensityAlpha: [T]) {
+        self.init(width: width, height: height, data: intensityAlpha)
+    }
+}
+
+extension Image where P == RGB {
+    public init(width: Int, height: Int, rgb: [T]) {
+        self.init(width: width, height: height, data: rgb)
+    }
+}
+
+extension Image where P == RGBA {
+    public init(width: Int, height: Int, rgba: [T]) {
+        self.init(width: width, height: height, data: rgba)
+    }
+}
+
+extension Image where P == ARGB {
+    public init(width: Int, height: Int, argb: [T]) {
+        self.init(width: width, height: height, data: argb)
+    }
+}
