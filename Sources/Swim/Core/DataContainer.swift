2a59bcf4b8ce27fe8c71a54b06bedcf7b14c37b0
araki <araki@qoncept.co.jp>
2018-02-02 16:41:30 +0900

    Refactor

--- Sources/Swim/Core/DataContainer.swift
+++ Sources/Swim/Core/DataContainer.swift
@@ -5,42 +5,34 @@ public protocol DataContainer {
     associatedtype PT: PixelType
     associatedtype DT: DataType
     
-    var count: Int { get }
+    // get-only to make deta internal(set)
+    var data: [DT] { get }
     
-    func withUnsafeBufferPointer<R>(_ body: (UnsafeBufferPointer<DT>) throws -> R) rethrows -> R
     mutating func withUnsafeMutableBufferPointer<R>(_ body: (inout UnsafeMutableBufferPointer<DT>) throws -> R) rethrows -> R
 }
 
-extension Image: DataContainer {
-    
-    public typealias PT = P
-    public typealias DT = T
-    
+extension DataContainer {
     public var count: Int {
         return data.count
     }
     
-    public func withUnsafeBufferPointer<R>(_ body: (UnsafeBufferPointer<T>) throws -> R) rethrows -> R {
+    public func withUnsafeBufferPointer<R>(_ body: (UnsafeBufferPointer<DT>) throws -> R) rethrows -> R {
         return try data.withUnsafeBufferPointer(body)
     }
+}
+
+extension Image: DataContainer {
+    public typealias PT = P
+    public typealias DT = T
     
     public mutating func withUnsafeMutableBufferPointer<R>(_ body: (inout UnsafeMutableBufferPointer<T>) throws -> R) rethrows -> R {
         return try data.withUnsafeMutableBufferPointer(body)
     }
-    
 }
 
 extension Pixel: DataContainer {
     public typealias PT = P
     public typealias DT = T
-    
-    public var count: Int {
-        return data.count
-    }
-    
-    public func withUnsafeBufferPointer<R>(_ body: (UnsafeBufferPointer<T>) throws -> R) rethrows -> R {
-        return try data.withUnsafeBufferPointer(body)
-    }
     
     public mutating func withUnsafeMutableBufferPointer<R>(_ body: (inout UnsafeMutableBufferPointer<T>) throws -> R) rethrows -> R{
         return try data.withUnsafeMutableBufferPointer(body)
