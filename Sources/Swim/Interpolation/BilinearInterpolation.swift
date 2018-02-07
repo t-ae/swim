7661e7d6d0a5357813ff0fd5e1ac52e695481d20
araki <araki@qoncept.co.jp>
2018-02-07 13:50:31 +0900

    Reduce pixel calculation

--- Sources/Swim/Interpolation/BilinearInterpolation.swift
+++ Sources/Swim/Interpolation/BilinearInterpolation.swift
@@ -14,20 +14,17 @@ extension Image where T: BinaryFloatingPoint {
         let yy1yp: T = T(yy+1) - y
         let ypyy: T = y - T(yy)
 
-        // interpolate left two pixels
-        var pl: Pixel<P, T> = yy1yp * self[unsafe: xx, yy]
-        pl += ypyy * self[unsafe: xx, yy1]
-
-        // interpolate right two pixels
-        var pr: Pixel<P, T> = yy1yp * self[unsafe: xx1, yy]
-        pr += ypyy * self[unsafe: xx1, yy1]
-
-        // interpolate two pixel
+        // left two pixels
         let xx1xp: T = T(xx+1) - x
-        pl *= xx1xp
-        let xpxx: T = x - T(xx)
-        pr *= xpxx
+        let tmp = xx1xp * yy1yp // somehow can't compile without this
+        var result: Pixel<P, T> = tmp * self[unsafe: xx, yy]
+        result += (xx1xp * ypyy) * self[unsafe: xx, yy1]
 
-        return pl + pr
+        // right two pixels
+        let xpxx: T = x - T(xx)
+        result += (xpxx * yy1yp) * self[unsafe: xx1, yy]
+        result += (xpxx * ypyy) * self[unsafe: xx1, yy1]
+
+        return result
     }
 }
