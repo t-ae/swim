0bbe25c0123f0b77e44aa5684f6c50137d27d634
araki <araki@qoncept.co.jp>
2019-04-21 10:23:06 +0900

    Add accelerate version multiplyBlend/addBlend

--- Sources/Swim/Blend/AddBlend.swift
+++ Sources/Swim/Blend/AddBlend.swift
@@ -2,7 +2,7 @@ import Foundation
 
 extension Blender {
     @inlinable
-    public static func addBlend<T: BinaryFloatingPoint>(top: Image<RGB, T>, bottom: inout Image<RGB, T>) {
+    public static func addBlend<T: FloatingPoint>(top: Image<RGB, T>, bottom: inout Image<RGB, T>) {
         precondition(top.size == bottom.size, "Images must have same size.")
         
         top.data.withUnsafeBufferPointer {
@@ -19,3 +19,47 @@ extension Blender {
         }
     }
 }
+
+#if canImport(Accelerate)
+
+import Accelerate
+
+extension Blender {
+    @inlinable
+    public static func addBlend(top: Image<RGB, Float>, bottom: inout Image<RGB, Float>) {
+        precondition(top.size == bottom.size, "Images must have same size.")
+        
+        top.data.withUnsafeBufferPointer {
+            let src = $0.baseAddress!
+            bottom.data.withUnsafeMutableBufferPointer {
+                let dst = $0.baseAddress!
+                
+                vDSP_vadd(src, 1, dst, 1, dst, 1, vDSP_Length($0.count))
+                var low: Float = 0
+                var high: Float = 1
+                vDSP_vclip(dst, 1, &low, &high, dst, 1, vDSP_Length($0.count))
+            }
+        }
+    }
+}
+
+extension Blender {
+    @inlinable
+    public static func addBlend(top: Image<RGB, Double>, bottom: inout Image<RGB, Double>) {
+        precondition(top.size == bottom.size, "Images must have same size.")
+        
+        top.data.withUnsafeBufferPointer {
+            let src = $0.baseAddress!
+            bottom.data.withUnsafeMutableBufferPointer {
+                let dst = $0.baseAddress!
+                
+                vDSP_vaddD(src, 1, dst, 1, dst, 1, vDSP_Length($0.count))
+                var low: Double = 0
+                var high: Double = 1
+                vDSP_vclipD(dst, 1, &low, &high, dst, 1, vDSP_Length($0.count))
+            }
+        }
+    }
+}
+
+#endif
