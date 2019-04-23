417971dbd88072897ba2381b70e7aa5edf535c10
araki <araki@qoncept.co.jp>
2019-04-23 14:45:31 +0900

    Add interleave test

--- Tests/PerformanceTests/BasicPerformanceTests.swift
+++ Tests/PerformanceTests/BasicPerformanceTests.swift
@@ -37,6 +37,56 @@ extension BasicPerformanceTests {
             }
         }
     }
+    
+    func testInterleave() {
+        let cnt = 1000000
+        let x = [Double](repeating: 0, count: cnt)
+        let y = [Double](repeating: 1, count: cnt)
+        measure {
+            var result = [Double](repeating: 0, count: 2*cnt)
+            x.withUnsafeBufferPointer {
+                var xp = $0.baseAddress!
+                y.withUnsafeBufferPointer {
+                    var yp = $0.baseAddress!
+                    result.withUnsafeMutableBufferPointer {
+                        var p = $0.baseAddress!
+                        
+                        for _ in 0..<cnt {
+                            p.pointee = xp.pointee
+                            p += 1
+                            xp += 1
+                            p.pointee = yp.pointee
+                            p += 1
+                            yp += 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func testInterleave_accelerate() {
+        let cnt = 1000000
+        let x = [Double](repeating: 0, count: cnt)
+        let y = [Double](repeating: 1, count: cnt)
+        measure {
+            var result = [Double](repeating: 0, count: 2*cnt)
+            result.withUnsafeMutableBufferPointer {
+                var p = $0.baseAddress!
+                x.withUnsafeBufferPointer {
+                    let xp = $0.baseAddress!
+                    
+                    cblas_dcopy(Int32(cnt), xp, 1, p, 2)
+                }
+                p += 1
+                y.withUnsafeBufferPointer {
+                    let yp = $0.baseAddress!
+                    
+                    cblas_dcopy(Int32(cnt), yp, 1, p, 2)
+                }
+            }
+        }
+    }
 }
 
 #endif
