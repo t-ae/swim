095ebfbe2545cf838a4f9335de850ea35b3655e7
araki <araki@qoncept.co.jp>
2019-04-25 18:11:14 +0900

    Reduce utility

--- Sources/Swim/Utility/ArrayExt.swift
+++ Sources/Swim/Utility/ArrayExt.swift
@@ -1,37 +1,11 @@
 extension Array where Element: Comparable {
     @inlinable
-    func minIndex() -> Int? {
-        guard let minValue = self.min() else {
-            return nil
-        }
-        return firstIndex(of: minValue)
-    }
-    
-    @inlinable
-    func maxIndex() -> Int? {
-        guard let maxValue = self.max() else {
-            return nil
-        }
-        return firstIndex(of: maxValue)
-    }
-}
-
-extension Array where Element: Comparable {
-    @inlinable
     func median() -> Element? {
         guard count > 0 else {
             return nil
         }
         let s = sorted()
         return s[(count-1)/2]
-    }
-    
-    @inlinable
-    func medianIndex() -> Int? {
-        guard let medianValue = median() else {
-            return nil
-        }
-        return firstIndex(of: medianValue)
     }
 }
 
