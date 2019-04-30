adc65a8e83d096e6d137a9a73030e44b16450b0e
araki <araki@qoncept.co.jp>
2019-04-30 13:47:50 +0900

    Refactor channel composition

--- Sources/Swim/Conversion/ChannelComposition.swift
+++ Sources/Swim/Conversion/ChannelComposition.swift
@@ -1,82 +1,49 @@
 import Foundation
 
-@inlinable
-func compoundChannels<T>(intensity: Image<Intensity, T>,
-                         alpha: Image<Intensity, T>) -> Image<IntensityAlpha, T> {
-    precondition(intensity.size == alpha.size, "Images must have same size.")
-    
-    let width = intensity.width
-    let height = alpha.height
-    let data = interleave(data1: intensity.data, data2: alpha.data)
-    
-    return Image(width: width, height: height, data: data)
-}
-
-@inlinable
-func compoundChannels<T>(r: Image<Intensity, T>,
-                         g: Image<Intensity, T>,
-                         b: Image<Intensity, T>) -> Image<RGB, T> {
-    precondition(r.size == g.size && g.size == b.size, "Images must have same size.")
-    
-    let width = r.width
-    let height = r.height
-    let data = interleave(data1: r.data, data2: g.data, data3: b.data)
-    
-    return Image(width: width, height: height, data: data)
-}
-
-@inlinable
-func compoundChannels<T>(r: Image<Intensity, T>,
-                         g: Image<Intensity, T>,
-                         b: Image<Intensity, T>,
-                         a: Image<Intensity, T>) -> Image<RGBA, T> {
-    precondition(r.size == g.size && g.size == b.size && b.size == a.size, "Images must have same size.")
-    
-    let width = r.width
-    let height = r.height
-    let data = interleave(data1: r.data, data2: g.data, data3: b.data, data4: a.data)
-    
-    return Image(width: width, height: height, data: data)
-}
-
-@inlinable
-func compoundChannels<T>(a: Image<Intensity, T>,
-                         r: Image<Intensity, T>,
-                         g: Image<Intensity, T>,
-                         b: Image<Intensity, T>) -> Image<ARGB, T> {
-    precondition(a.size == r.size && r.size == g.size && g.size == b.size, "Images must have same size.")
-    
-    let width = a.width
-    let height = a.height
-    let data = interleave(data1: a.data, data2: r.data, data3: g.data, data4: b.data)
-    
-    return Image(width: width, height: height, data: data)
-}
-
 extension Image where P == IntensityAlpha {
     @inlinable
     public init(intensity: Image<Intensity, T>, alpha: Image<Intensity, T>) {
-        self = compoundChannels(intensity: intensity, alpha: alpha)
+        precondition(intensity.size == alpha.size, "Images must have same size.")
+        
+        let (width, height) = intensity.size
+        let data = interleave(data1: intensity.data, data2: alpha.data)
+        
+        self.init(width: width, height: height, data: data)
     }
 }
 
 extension Image where P == RGB {
     @inlinable
     public init(r: Image<Intensity, T>, g: Image<Intensity, T>, b: Image<Intensity, T>) {
-        self = compoundChannels(r: r, g: g, b: b)
+        precondition(r.size == g.size && g.size == b.size, "Images must have same size.")
+        
+        let (width, height) = r.size
+        let data = interleave(data1: r.data, data2: g.data, data3: b.data)
+        
+        self.init(width: width, height: height, data: data)
     }
 }
 
 extension Image where P == RGBA {
     @inlinable
     public init(r: Image<Intensity, T>, g: Image<Intensity, T>, b: Image<Intensity, T>, a: Image<Intensity, T>) {
-        self = compoundChannels(r: r, g: g, b: b, a: a)
+        precondition(r.size == g.size && g.size == b.size && b.size == a.size, "Images must have same size.")
+        
+        let (width, height) = r.size
+        let data = interleave(data1: r.data, data2: g.data, data3: b.data, data4: a.data)
+        
+        self.init(width: width, height: height, data: data)
     }
 }
 
 extension Image where P == ARGB {
     @inlinable
     public init(a: Image<Intensity, T>, r: Image<Intensity, T>, g: Image<Intensity, T>, b: Image<Intensity, T>) {
-        self = compoundChannels(a: a, r: r, g: g, b: b)
+        precondition(a.size == r.size && r.size == g.size && g.size == b.size, "Images must have same size.")
+        
+        let (width, height) = r.size
+        let data = interleave(data1: a.data, data2: r.data, data3: g.data, data4: b.data)
+        
+        self.init(width: width, height: height, data: data)
     }
 }
