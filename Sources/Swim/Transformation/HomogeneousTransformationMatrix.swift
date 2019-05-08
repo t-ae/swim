d1a1a3e2ce5e2f2b7d780d09f80a448f558b6503
araki <araki@qoncept.co.jp>
2019-05-08 15:35:55 +0900

    Move projective transformation

--- Sources/Swim/Transformation/HomogeneousTransformationMatrix.swift
+++ Sources/Swim/Transformation/HomogeneousTransformationMatrix.swift
@@ -1,8 +1,7 @@
 // MARK: - HomogeneousTransformationMatrixProtocol
 
 public protocol HomogeneousTransformationMatrixProtocol {
-    associatedtype InvertedMatrix: HomogeneousTransformationMatrixProtocol
-    @inlinable func inverted() throws -> InvertedMatrix
+    @inlinable func inverted() throws -> Self
     @inlinable static func *(lhs: Self, rhs: (x: Double, y: Double)) -> (x: Double, y: Double)
 }
 
@@ -48,15 +47,56 @@ public struct HomogeneousTransformationMatrix: HomogeneousTransformationMatrixPr
         let e21: Double = e[1]*e[6] - e[0]*e[7]
         let e22: Double = e[0]*e[4] - e[1]*e[3]
         
-        return HomogeneousTransformationMatrix(elements: [e00 / det, e01 / det, e02 / det,
-                                                          e10 / det, e11 / det, e12 / det,
-                                                          e20 / det, e21 / det, e22 / det])
+        return HomogeneousTransformationMatrix(elements: [e00, e01, e02,
+                                                          e10, e11, e12,
+                                                          e20, e21, e22].map { $0/det })
     }
 }
 
 extension HomogeneousTransformationMatrix {
     public var identity: HomogeneousTransformationMatrix {
         return .init(elements: [1, 0, 0, 0, 1, 0, 0, 0, 1])
+    }
+}
+
+extension HomogeneousTransformationMatrix {
+    public static func project(from: ((Double, Double), (Double, Double), (Double, Double), (Double, Double)),
+                               to: ((Double, Double), (Double, Double), (Double, Double), (Double, Double))) throws -> HomogeneousTransformationMatrix {
+        // a: (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 1) to `from`
+        let a: HomogeneousTransformationMatrix
+        do {
+            let mat = HomogeneousTransformationMatrix(elements: [from.0.0, from.1.0, from.2.0,
+                                                                 from.0.1, from.1.1, from.2.1,
+                                                                 1, 1, 1])
+            let inv = try mat.inverted().elements
+            let coef0 = inv[0]*from.3.0 + inv[1]*from.3.1 + inv[2]
+            let coef1 = inv[3]*from.3.0 + inv[4]*from.3.1 + inv[5]
+            let coef2 = inv[6]*from.3.0 + inv[7]*from.3.1 + inv[8]
+            
+            a = HomogeneousTransformationMatrix(elements: [
+                coef0*from.0.0, coef1*from.1.0, coef2*from.2.0,
+                coef0*from.0.1, coef1*from.1.1, coef2*from.2.1,
+                coef0, coef1, coef2])
+        }
+        // b: (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 1) to `to`
+        let b: HomogeneousTransformationMatrix
+        do {
+            let mat = HomogeneousTransformationMatrix(elements: [to.0.0, to.1.0, to.2.0,
+                                                                 to.0.1, to.1.1, to.2.1,
+                                                                 1, 1, 1])
+            let inv = try mat.inverted().elements
+            let coef0 = inv[0]*to.3.0 + inv[1]*to.3.1 + inv[2]
+            let coef1 = inv[3]*to.3.0 + inv[4]*to.3.1 + inv[5]
+            let coef2 = inv[6]*to.3.0 + inv[7]*to.3.1 + inv[8]
+            
+            b = HomogeneousTransformationMatrix(elements: [
+                coef0*to.0.0, coef1*to.1.0, coef2*to.2.0,
+                coef0*to.0.1, coef1*to.1.1, coef2*to.2.1,
+                coef0, coef1, coef2])
+        }
+        
+        // a.inverted: `from` to (1, 0, 0), (0, 1, 0), (0, 0, 1), (1, 1, 1)
+        return try b * a.inverted()
     }
 }
 
--- Tests/PerformanceTests/ResizePerformanceTests.swift
+++ Tests/PerformanceTests/ResizePerformanceTests.swift
@@ -2,6 +2,15 @@ import XCTest
 import Swim
 
 class ResizePerformanceTests: XCTestCase {
+    func testResizeNN_UInt8() {
+        let data = [UInt8](repeating: 0, count: 64*48)
+        let image = Image(width: 64, height: 48, intensity: data)
+        
+        measure {
+            _ = image.resizeNN(width: 1920, height: 1080)
+        }
+    }
+    
     func testResizeAA() {
         let data = [Float](repeating: 0, count: 1920*1080)
         let image = Image(width: 1920, height: 1080, intensity: data)
