96d182862cf064ca45334a84c2e1a5627108bcdf
araki <araki@qoncept.co.jp>
2019-05-20 12:56:14 +0900

    Add mask for correlations

--- Sources/Swim/Advance/Correlation.swift
+++ Sources/Swim/Advance/Correlation.swift
@@ -7,11 +7,15 @@ public enum Correlation<T: DataType> {
 extension Correlation where T: Strideable {
     /// Compute sum of square difference.
     @inlinable
-    public static func ssd(_ a: Image<Gray, T>, _ b: Image<Gray, T>) -> T.Stride {
+    public static func ssd(_ a: Image<Gray, T>, _ b: Image<Gray, T>, mask: Image<Gray, Bool>? = nil) -> T.Stride {
         precondition(a.size == b.size, "Images must have same size.")
+        precondition(a.size == mask?.size ?? a.size, "Mask must have same size as images.")
         
         var sum: T.Stride = 0
         for i in 0..<a.data.count {
+            guard mask?.data[i] ?? true else {
+                continue
+            }
             let distance = a.data[i].distance(to: b.data[i])
             sum += distance*distance
         }
@@ -20,11 +24,15 @@ extension Correlation where T: Strideable {
     
     /// Compute sum of square difference.
     @inlinable
-    public static func sad(_ a: Image<Gray, T>, _ b: Image<Gray, T>) -> T.Stride {
+    public static func sad(_ a: Image<Gray, T>, _ b: Image<Gray, T>, mask: Image<Gray, Bool>? = nil) -> T.Stride {
         precondition(a.size == b.size, "Images must have same size.")
+        precondition(a.size == mask?.size ?? a.size, "Mask must have same size as images.")
         
         var sum: T.Stride = 0
         for i in 0..<a.data.count {
+            guard mask?.data[i] ?? true else {
+                continue
+            }
             sum += abs(a.data[i].distance(to: b.data[i]))
         }
         return sum
@@ -35,14 +43,18 @@ extension Correlation where T: FloatingPoint {
     /// Compute normalized cross correlation.
     /// - Precondition: Each of `a` and `b` have at least one non-zero value.
     @inlinable
-    public static func ncc(_ a: Image<Gray, T>, _ b: Image<Gray, T>) -> T {
+    public static func ncc(_ a: Image<Gray, T>, _ b: Image<Gray, T>, mask: Image<Gray, Bool>? = nil) -> T {
         precondition(a.size == b.size, "Images must have same size.")
+        precondition(a.size == mask?.size ?? a.size, "Mask must have same size as images.")
         
         var sum2a: T = 0
         var sum2b: T = 0
         var sumCross: T = 0
         
         for i in 0..<a.data.count {
+            guard mask?.data[i] ?? true else {
+                continue
+            }
             sum2a += a.data[i]*a.data[i]
             sum2b += b.data[i]*b.data[i]
             sumCross += a.data[i]*b.data[i]
@@ -57,24 +69,30 @@ extension Correlation where T: FloatingPoint {
     /// Compute zero means normalized cross correlation.
     /// - Precondition: Each of `a` and `b` can't have same value in all pixels.
     @inlinable
-    public static func zncc(_ a: Image<Gray, T>, _ b: Image<Gray, T>) -> T {
+    public static func zncc(_ a: Image<Gray, T>, _ b: Image<Gray, T>, mask: Image<Gray, Bool>? = nil) -> T {
         precondition(a.size == b.size, "Images must have same size.")
+        precondition(a.size == mask?.size ?? a.size, "Mask must have same size as images.")
         
         var suma: T = 0
         var sumb: T = 0
         var sum2a: T = 0
         var sum2b: T = 0
         var sumCross: T = 0
+        var count = 0
         
         for i in 0..<a.data.count {
+            guard mask?.data[i] ?? true else {
+                continue
+            }
             suma += a.data[i]
             sumb += b.data[i]
             sum2a += a.data[i]*a.data[i]
             sum2b += b.data[i]*b.data[i]
             sumCross += a.data[i]*b.data[i]
+            count += 1
         }
         
-        let c: T = T(a.width * a.height)
+        let c = T(count)
         let sumasumb = suma*sumb
         let suma2 = suma*suma
         let sumb2 = sumb*sumb
--- Tests/PerformanceTests/CorrelationPerformanceTests.swift
+++ Tests/PerformanceTests/CorrelationPerformanceTests.swift
@@ -14,6 +14,19 @@ class CorrelationPerformanceTests: XCTestCase {
             }
         }
     }
+    
+    func testMaskedSSD() {
+        let image1 = Image<Gray, Double>(width: 1920, height: 1080, value: 0)
+        let image2 = Image<Gray, Double>(width: 1920, height: 1080, value: 0)
+        let mask = Image<Gray, Bool>(width: 1920, height: 1080, value: true)
+        
+        measure {
+            for _ in 0..<100 {
+                let d = Correlation.ssd(image1, image2, mask: mask)
+                XCTAssertEqual(d, 0)
+            }
+        }
+    }
 
     func testSAD() {
         let image1 = Image<Gray, Double>(width: 1920, height: 1080, value: 0)
@@ -22,6 +35,19 @@ class CorrelationPerformanceTests: XCTestCase {
         measure {
             for _ in 0..<100 {
                 let d = Correlation.sad(image1, image2)
+                XCTAssertEqual(d, 0)
+            }
+        }
+    }
+    
+    func testMaskedSAD() {
+        let image1 = Image<Gray, Double>(width: 1920, height: 1080, value: 0)
+        let image2 = Image<Gray, Double>(width: 1920, height: 1080, value: 0)
+        let mask = Image<Gray, Bool>(width: 1920, height: 1080, value: true)
+        
+        measure {
+            for _ in 0..<100 {
+                let d = Correlation.sad(image1, image2, mask: mask)
                 XCTAssertEqual(d, 0)
             }
         }
@@ -39,6 +65,19 @@ class CorrelationPerformanceTests: XCTestCase {
         }
     }
     
+    func testMaskedNCC() {
+        let image1 = Image<Gray, Double>(width: 1920, height: 1080, value: 0.01)
+        let image2 = Image<Gray, Double>(width: 1920, height: 1080, value: 0.01)
+        let mask = Image<Gray, Bool>(width: 1920, height: 1080, value: true)
+        
+        measure {
+            for _ in 0..<100 {
+                let d = Correlation.ncc(image1, image2, mask: mask)
+                XCTAssertEqual(d, 1)
+            }
+        }
+    }
+    
     func testZNCC() {
         let image1 = Image<Gray, Double>(width: 1920, height: 1080, value: 0.01)
         let image2 = Image<Gray, Double>(width: 1920, height: 1080, value: 0.02)
@@ -46,6 +85,19 @@ class CorrelationPerformanceTests: XCTestCase {
         measure {
             for _ in 0..<100 {
                 let d = Correlation.zncc(image1, image2)
+                XCTAssertEqual(d, 1, accuracy: 1e-3)
+            }
+        }
+    }
+    
+    func testMaskedZNCC() {
+        let image1 = Image<Gray, Double>(width: 1920, height: 1080, value: 0.01)
+        let image2 = Image<Gray, Double>(width: 1920, height: 1080, value: 0.02)
+        let mask = Image<Gray, Bool>(width: 1920, height: 1080, value: true)
+        
+        measure {
+            for _ in 0..<100 {
+                let d = Correlation.zncc(image1, image2, mask: mask)
                 XCTAssertEqual(d, 1, accuracy: 1e-3)
             }
         }
