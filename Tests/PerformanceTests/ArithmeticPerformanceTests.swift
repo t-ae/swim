571e2720840530d66388ef4192621d3cc6eb90ec
araki <araki@qoncept.co.jp>
2019-05-27 17:11:46 +0900

    Change test

--- Tests/PerformanceTests/ArithmeticPerformanceTests.swift
+++ Tests/PerformanceTests/ArithmeticPerformanceTests.swift
@@ -2,86 +2,29 @@ import XCTest
 import Swim
 
 class ArithmeticPerformanceTests: XCTestCase {
-    func testAddScalarUInt8() {
-        let image = Image<RGBA, UInt8>(width: 3840, height: 2160, value: 0)
+    func testMulScalar() {
+        var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
         
         measure {
-            _ = image + 1
+            image *= 1.1
         }
     }
     
-    func testAddScalarFloat() {
-        let image = Image<RGBA, Float>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = image + 1
-        }
-    }
-    
-    func testAddScalarDouble() {
-        let image = Image<RGBA, Double>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = image + 1
-        }
-    }
-    
-    func testScalarMinusUInt8() {
-        let image = Image<RGBA, UInt8>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = 255 - image
-        }
-    }
-    func testScalarMinusFloat() {
-        let image = Image<RGBA, Float>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = 1 - image
-        }
-    }
-    
-    func testScalarMinusDouble() {
-        let image = Image<RGBA, Double>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = 1 - image
-        }
-    }
-    
-    func testAddImageUInt8() {
-        let image1 = Image<RGBA, UInt8>(width: 3840, height: 2160, value: 0)
-        let image2 = Image<RGBA, UInt8>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = image1 + image2
-        }
-    }
-    
-    func testAddImageFloat() {
-        let image1 = Image<RGBA, Float>(width: 3840, height: 2160, value: 0)
-        let image2 = Image<RGBA, Float>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = image1 + image2
-        }
-    }
-    
-    func testAddImageDouble() {
-        let image1 = Image<RGBA, Double>(width: 3840, height: 2160, value: 0)
-        let image2 = Image<RGBA, Double>(width: 3840, height: 2160, value: 0)
-        
-        measure {
-            _ = image1 + image2
-        }
-    }
-    
-    func testMulPixelDouble2() {
-        var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 0)
-        let pixel = Pixel<RGBA, Double>.red
+    func testMulPixel() {
+        var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
+        let pixel = Pixel<RGBA, Double>(r: 0.9, g: 1.1, b: 0.9, a: 1.1)
         
         measure {
             image *= pixel
+        }
+    }
+    
+    func testMulImage() {
+        var image1 = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
+        let image2 = Image<RGBA, Double>(width: 3840, height: 2160, value: 1.1)
+        
+        measure {
+            image1 *= image2
         }
     }
 }
