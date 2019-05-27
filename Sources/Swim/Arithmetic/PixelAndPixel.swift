49685bf58c6ab71ebe0260078564f8adba121d64
araki <araki@qoncept.co.jp>
2019-05-27 17:44:25 +0900

    Fix: rest pixel binary operators

--- Sources/Swim/Arithmetic/PixelAndPixel.swift
+++ Sources/Swim/Arithmetic/PixelAndPixel.swift
@@ -17,10 +17,8 @@ extension Pixel where T: AdditiveArithmetic {
     
     @inlinable
     public static func -=<Px: PixelProtocol>(lhs: inout Pixel, rhs: Px) where Px.P == P, Px.T == T {
-        rhs.withUnsafeBufferPointer { rbp in
-            for i in 0..<lhs.data.count {
-                lhs.data[i] -= rbp[i]
-            }
+        for i in 0..<lhs.data.count {
+            lhs.data[i] -= rhs[i]
         }
     }
     
@@ -35,10 +33,8 @@ extension Pixel where T: AdditiveArithmetic {
 extension Pixel where T: Numeric {
     @inlinable
     public static func *=<Px: PixelProtocol>(lhs: inout Pixel, rhs: Px) where Px.P == P, Px.T == T {
-        rhs.withUnsafeBufferPointer { rbp in
-            for i in 0..<lhs.data.count {
-                lhs.data[i] *= rbp[i]
-            }
+        for i in 0..<lhs.data.count {
+            lhs.data[i] *= rhs[i]
         }
     }
     
@@ -53,10 +49,8 @@ extension Pixel where T: Numeric {
 extension Pixel where T: BinaryInteger {
     @inlinable
     public static func /=<Px: PixelProtocol>(lhs: inout Pixel, rhs: Px) where Px.P == P, Px.T == T {
-        rhs.withUnsafeBufferPointer { rbp in
-            for i in 0..<lhs.data.count {
-                lhs.data[i] /= rbp[i]
-            }
+        for i in 0..<lhs.data.count {
+            lhs.data[i] /= rhs[i]
         }
     }
     
@@ -71,10 +65,8 @@ extension Pixel where T: BinaryInteger {
 extension Pixel where T: BinaryFloatingPoint {
     @inlinable
     public static func /=<Px: PixelProtocol>(lhs: inout Pixel, rhs: Px) where Px.P == P, Px.T == T {
-        rhs.withUnsafeBufferPointer { rbp in
-            for i in 0..<lhs.data.count {
-                lhs.data[i] /= rbp[i]
-            }
+        for i in 0..<lhs.data.count {
+            lhs.data[i] /= rhs[i]
         }
     }
     
