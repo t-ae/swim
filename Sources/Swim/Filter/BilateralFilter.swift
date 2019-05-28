c4ad4b0ac6cd0d7981661017ff4d9f2f5c964f5b
araki <araki@qoncept.co.jp>
2019-05-28 16:27:45 +0900

    Change bilateralFilter, nonLocalMeanFilter

--- Sources/Swim/Filter/BilateralFilter.swift
+++ Sources/Swim/Filter/BilateralFilter.swift
@@ -26,34 +26,32 @@ extension Image where T == Double {
             ref[.gray] = exp(-Double(dx*dx + dy+dy) / (2*sigma2_1))
         }
         
-        newImage.pixelwiseConvert { ref in
-            for c in 0..<P.channels {
-                let centerValue = self[ref.x, ref.y, c]
+        newImage.channelwiseConvert { x, y, c, value in
+            let centerValue = self[x, y, c]
+            
+            var denominator: Double = 0
+            var numerator: Double = 0
+            
+            for py in 0..<kernelSize {
+                let yy = clamp(y + py - pad, min: 0, max: height-1)
                 
-                var denominator: Double = 0
-                var numerator: Double = 0
-                
-                for py in 0..<kernelSize {
-                    let yy = clamp(ref.y + py - pad, min: 0, max: height-1)
+                for px in 0..<kernelSize {
+                    let xx = clamp(x + px - pad, min: 0, max: width-1)
                     
-                    for px in 0..<kernelSize {
-                        let xx = clamp(ref.x + px - pad, min: 0, max: width-1)
-                        
-                        let distanceGauss = distanceLUT[px, py, .gray]
-                        
-                        let pixelValue = self[xx, yy, c]
-                        let diff = pixelValue - centerValue
-                        let valueGauss = exp(-diff*diff / (2*sigma2_2))
-                        
-                        let prod = distanceGauss * valueGauss
-                        
-                        denominator += pixelValue * prod
-                        numerator += prod
-                    }
+                    let distanceGauss = distanceLUT[px, py, .gray]
+                    
+                    let pixelValue = self[xx, yy, c]
+                    let diff = pixelValue - centerValue
+                    let valueGauss = exp(-diff*diff / (2*sigma2_2))
+                    
+                    let prod = distanceGauss * valueGauss
+                    
+                    denominator += pixelValue * prod
+                    numerator += prod
                 }
-                
-                ref[c] = denominator / numerator
             }
+            
+            return denominator / numerator
         }
         
         return newImage
--- Sources/Swim/Filter/NonLocalMeanFilter.swift
+++ Sources/Swim/Filter/NonLocalMeanFilter.swift
@@ -17,40 +17,39 @@ extension Image where T == Double {
         
         let pad = (kernelSize-1)/2
         
-        newImage.pixelwiseConvert { ref in
-            for c in 0..<P.channels {
-                var z: Double = 0 // sum of weights
-                
-                for dy in 0..<kernelSize {
-                    let ly = ref.y + dy - pad
-                    for dx in 0..<kernelSize {
-                        let lx = ref.x + dx - pad
-                        
-                        // Compute distance^2
-                        var distance2: Double = 0
-                        for i in 0..<kernelSize {
-                            let yy1 = clamp(ref.y + i - pad, min: 0, max: height-1)
-                            let yy2 = clamp(ly + i - pad, min: 0, max: height-1)
-                            for j in 0..<kernelSize {
-                                let xx1 = clamp(ref.x + j - pad, min: 0, max: width-1)
-                                let xx2 = clamp(lx + j - pad, min: 0, max: width-1)
-                                
-                                let s = self[xx1, yy1, c]
-                                let l = self[xx2, yy2, c]
-                                distance2 += s*s + l*l - 2*s*l
-                            }
+        newImage.channelwiseConvert { x, y, c, value in
+            var weightedSum: Double = 0
+            var z: Double = 0 // sum of weights
+            
+            for dy in 0..<kernelSize {
+                let ly = y + dy - pad
+                for dx in 0..<kernelSize {
+                    let lx = x + dx - pad
+                    
+                    // Compute distance^2
+                    var distance2: Double = 0
+                    for i in 0..<kernelSize {
+                        let yy1 = clamp(y + i - pad, min: 0, max: height-1)
+                        let yy2 = clamp(ly + i - pad, min: 0, max: height-1)
+                        for j in 0..<kernelSize {
+                            let xx1 = clamp(x + j - pad, min: 0, max: width-1)
+                            let xx2 = clamp(lx + j - pad, min: 0, max: width-1)
+                            
+                            let s = self[xx1, yy1, c]
+                            let l = self[xx2, yy2, c]
+                            distance2 += s*s + l*l - 2*s*l
                         }
-                        
-                        let w = exp(-distance2 / sigma2)
-                        z += w
-                        
-                        ref[c] += w * self[clamp(lx, min: 0, max: width-1), clamp(ly, min: 0, max: height-1), c]
                     }
+                    
+                    let w = exp(-distance2 / sigma2)
+                    z += w
+                    
+                    weightedSum += w * self[clamp(lx, min: 0, max: width-1), clamp(ly, min: 0, max: height-1), c]
                 }
-                
-                // normalize
-                ref[c] /= z
             }
+            
+            // normalize
+            return weightedSum / z
         }
         
         return newImage
