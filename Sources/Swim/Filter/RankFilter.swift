3a0ae60eddda49b49ba13d9a0127318a90b80022
araki <araki@qoncept.co.jp>
2019-05-29 15:52:07 +0900

    Change rankFilter

--- Sources/Swim/Filter/RankFilter.swift
+++ Sources/Swim/Filter/RankFilter.swift
@@ -42,29 +42,22 @@ extension Image where T: Comparable {
     @inlinable
     func rankFilter(kernelSize: Int, kernelFunc: (Slice<UnsafeMutableBufferPointer<T>>)->T) -> Image<P, T> {
         precondition(kernelSize > 0, "kernelSize must be greater than 0.")
-        var newImage = self
         
         let pad = (kernelSize - 1) / 2
         
         var patch = [T](repeating: T.swimDefaultValue, count: kernelSize*kernelSize)
         
-        patch.withUnsafeMutableBufferPointer { patch in
-            for c in 0..<P.channels {
-                for y in 0..<height {
-                    for x in 0..<width {
-                        var count = 0
-                        for yy in max(y-pad, 0)..<min(y-pad+kernelSize, height) {
-                            for xx in max(x-pad, 0)..<min(x-pad+kernelSize, width) {
-                                patch[count] = self[xx, yy, c]
-                                count += 1
-                            }
-                        }
-                        newImage[x, y, c] = kernelFunc(patch[..<count])
+        return patch.withUnsafeMutableBufferPointer { patch in
+            .createWithPixelValues(width: width, height: height) { x, y, c in
+                var count = 0
+                for yy in max(y-pad, 0)..<min(y-pad+kernelSize, height) {
+                    for xx in max(x-pad, 0)..<min(x-pad+kernelSize, width) {
+                        patch[count] = self[xx, yy, c]
+                        count += 1
                     }
                 }
+                return kernelFunc(patch[..<count])
             }
         }
-        
-        return newImage
     }
 }
