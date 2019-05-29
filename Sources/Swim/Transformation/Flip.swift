9da819966d496c89050ea4ee87a94db1e8d142e5
araki <araki@qoncept.co.jp>
2019-05-29 13:32:44 +0900

    Change flip*

--- Sources/Swim/Transformation/Flip.swift
+++ Sources/Swim/Transformation/Flip.swift
@@ -4,14 +4,10 @@ extension Image {
     @inlinable
     public func flipLR() -> Image<P, T> {
         return Image.createWithUnsafeMutableBufferPointer(width: width, height: height) { dst in
-            var srcOffset = 0
-            for y in 0..<height {
-                var dstOffset = (y+1)*width*P.channels - P.channels
-                for _ in 0..<width {
-                    copy(src: data, srcOffset: srcOffset,
-                         dst: dst, dstOffset: dstOffset, count: P.channels)
-                    srcOffset += P.channels
-                    dstOffset -= P.channels
+            for px in self.pixels() {
+                let dstOffset = Image<P, T>.dataIndex(x: width - 1 - px.x, y: px.y, width: width, height: height)
+                px.color.withUnsafeBufferPointer { bp in
+                    copy(src: bp, dst: dst, dstOffset: dstOffset, count: P.channels)
                 }
             }
         }
@@ -21,7 +17,8 @@ extension Image {
     public func flipUD() -> Image<P, T> {
         return Image.createWithUnsafeMutableBufferPointer(width: width, height: height) { dst in
             var srcOffset = 0
-            var dstOffset = (height-1)*width*P.channels
+            var dstOffset = Image<P, T>.dataIndex(x: 0, y: height-1, width: width, height: height)
+            
             for  _ in 0..<height {
                 copy(src: data, srcOffset: srcOffset,
                      dst: dst, dstOffset: dstOffset, count: width*P.channels)
