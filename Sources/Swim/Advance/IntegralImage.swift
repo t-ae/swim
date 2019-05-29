db866c8aa8ecbdaec8aa3b7bcf29c1657c3db5b4
araki <araki@qoncept.co.jp>
2019-05-29 17:26:17 +0900

    Replace IntegralImageConverter.convert

--- Sources/Swim/Advance/IntegralImage.swift
+++ Sources/Swim/Advance/IntegralImage.swift
@@ -2,52 +2,48 @@ public enum IntegralImageConverter {
     /// Create integral image from input image.
     @inlinable
     public static func convert<T: BinaryInteger>(image: Image<Gray, T>) -> Image<Gray, Int> {
-        var newImage = Image<Gray, Int>.zeros(like: image)
-        
-        // First row
-        do {
-            var rowsum = 0
-            for i in 0..<image.width {
-                rowsum += Int(image.data[i])
-                newImage.data[i] = rowsum
+        return .createWithUnsafeMutableBufferPointer(width: image.width, height: image.height) { bp in
+            // First row
+            do {
+                var rowsum = 0
+                for i in 0..<image.width {
+                    rowsum += Int(image.data[i])
+                    bp[i] = rowsum
+                }
+            }
+            
+            // Rest rows
+            for i in stride(from: image.width, to: image.data.count, by: image.width) {
+                var rowsum = 0
+                for x in 0..<image.width {
+                    rowsum += Int(image.data[i+x])
+                    bp[i+x] = rowsum + bp[i+x-image.width]
+                }
             }
         }
-        
-        // Rest rows
-        for i in stride(from: image.width, to: image.data.count, by: image.width) {
-            var rowsum = 0
-            for x in 0..<image.width {
-                rowsum += Int(image.data[i+x])
-                newImage.data[i+x] = rowsum + newImage.data[i+x-newImage.width]
-            }
-        }
-        
-        return newImage
     }
     
     /// Create integral image from input image.
     @inlinable
     public static func convert<T: FloatingPoint>(image: Image<Gray, T>) -> Image<Gray, T> {
-        var newImage = Image.zeros(like: image)
-        
-        // First row
-        do {
-            var rowsum = T.zero
-            for i in 0..<image.width {
-                rowsum += image.data[i]
-                newImage.data[i] = rowsum
+        return .createWithUnsafeMutableBufferPointer(width: image.width, height: image.height) { bp in
+            // First row
+            do {
+                var rowsum = T.zero
+                for i in 0..<image.width {
+                    rowsum += image.data[i]
+                    bp[i] = rowsum
+                }
+            }
+            
+            // Rest rows
+            for i in stride(from: image.width, to: image.data.count, by: image.width) {
+                var rowsum = T.zero
+                for x in 0..<image.width {
+                    rowsum += image.data[i+x]
+                    bp[i+x] = rowsum + bp[i+x-image.width]
+                }
             }
         }
-        
-        // Rest rows
-        for i in stride(from: image.width, to: image.data.count, by: image.width) {
-            var rowsum = T.zero
-            for x in 0..<image.width {
-                rowsum += image.data[i+x]
-                newImage.data[i+x] = rowsum + newImage.data[i+x-newImage.width]
-            }
-        }
-        
-        return newImage
     }
 }
