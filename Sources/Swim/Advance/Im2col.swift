e122a5668136b52893e38db0a2688e34dad42f7c
araki <araki@qoncept.co.jp>
2019-05-31 10:16:27 +0900

    Add im2row

--- Sources/Swim/Advance/Im2col.swift
+++ Sources/Swim/Advance/Im2col.swift
@@ -1,8 +1,6 @@
 extension Image where P == Gray {
     /// im2col.
     ///
-    /// Each column in returned matrix corresponds to patch.
-    ///
     /// - Returns:
     ///   - matrix: Each column is patch.
     ///
@@ -28,6 +26,41 @@ extension Image where P == Gray {
                     for x in 0..<patchWidth {
                         let valueIndex = y * patchWidth + x
                         matrix[valueIndex*cols + patchIndex] = self[x+patchX, y+patchY, 0]
+                    }
+                }
+            }
+        }
+        
+        return Matrix(rows: rows, cols: cols, data: matrix)
+    }
+    
+    /// im2row.
+    ///
+    /// - Returns:
+    ///   - matrix: Each row is patch.
+    ///
+    /// - Note: This method doesn't support padding.
+    /// If you need padding, use `withPadding` beforehand.
+    @inlinable
+    public func im2row(patchWidth: Int,
+                       patchHeight: Int) -> Matrix<T> {
+        
+        let numPatchX = width-patchWidth+1
+        let numPatchY = height-patchHeight+1
+        
+        let rows = numPatchX * numPatchY
+        let cols = patchWidth*patchHeight
+        
+        var matrix = [T](repeating: T.swimDefaultValue, count: rows*cols)
+        
+        for patchY in 0..<numPatchY {
+            for patchX in 0..<numPatchX {
+                let patchIndex = patchY*numPatchX + patchX
+                
+                for y in 0..<patchHeight {
+                    for x in 0..<patchWidth {
+                        let valueIndex = y * patchWidth + x
+                        matrix[patchIndex*cols + valueIndex] = self[x+patchX, y+patchY, 0]
                     }
                 }
             }
--- Tests/SwimTests/AdvanceTests/Im2colTests.swift
+++ Tests/SwimTests/AdvanceTests/Im2colTests.swift
@@ -34,7 +34,37 @@ class Im2colTests: XCTestCase {
         }
     }
     
+    func testIm2Row() {
+        do {
+            let image = Image(width: 3, height: 2, gray: (0..<6).map { Float($0) })
+            let matrix = image.im2row(patchWidth: 2, patchHeight: 2)
+            XCTAssertEqual(matrix, Matrix(rows: 2, cols: 4, data: [0, 1, 3, 4,
+                                                                   1, 2, 4, 5]))
+            
+        }
+        do {
+            let image = Image(width: 3, height: 4, gray: (0..<12).map { Float($0) })
+            let matrix = image.im2row(patchWidth: 3, patchHeight: 2)
+            XCTAssertEqual(matrix, Matrix(rows: 3, cols: 6, data: [0, 1, 2, 3, 4, 5,
+                                                                   3, 4, 5, 6, 7, 8,
+                                                                   6, 7, 8, 9, 10, 11]))
+            
+        }
+        do {
+            let image = Image(width: 4, height: 4, gray: (0..<16).map { Float($0) })
+            let matrix = image.im2row(patchWidth: 3, patchHeight: 2)
+            XCTAssertEqual(matrix, Matrix(rows: 6, cols: 6, data: [0, 1, 2, 4, 5 ,6,
+                                                                   1, 2, 3, 5, 6, 7,
+                                                                   4, 5, 6, 8, 9, 10,
+                                                                   5, 6, 7, 9, 10, 11,
+                                                                   8, 9, 10, 12, 13, 14,
+                                                                   9, 10, 11, 13, 14, 15]))
+            
+        }
+    }
+    
     static let allTests = [
-        ("testIm2Col", testIm2Col)
+        ("testIm2Col", testIm2Col),
+        ("testIm2Row", testIm2Row)
     ]
 }
