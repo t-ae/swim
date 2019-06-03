06ab5991c23522c4430751bdac2071987d370ea3
araki <araki@qoncept.co.jp>
2019-06-03 10:43:09 +0900

    Add tests

--- Tests/PerformanceTests/ManipulationPerformanceTests.swift
+++ Tests/PerformanceTests/ManipulationPerformanceTests.swift
@@ -2,6 +2,13 @@ import XCTest
 import Swim
 
 class ManipulationPerformanceTests: XCTestCase {
+    func testAbs() {
+        var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
+        
+        measure {
+            image.applyAbs()
+        }
+    }
 
     func testFloor() {
         var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
@@ -26,12 +33,28 @@ class ManipulationPerformanceTests: XCTestCase {
             image.applyRound()
         }
     }
+    
+    func testTrunc() {
+        var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
+        
+        measure {
+            image.applyTrunc()
+        }
+    }
 
     func testClip() {
         var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
         
         measure {
             image.clip(low: 0, high: 1)
+        }
+    }
+    
+    func testPow() {
+        var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
+        
+        measure {
+            image.applyPow(2)
         }
     }
 }
