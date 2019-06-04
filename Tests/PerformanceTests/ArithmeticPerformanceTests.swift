7d28ebaa46fb6c991f7a251d32a371c72366c889
araki <araki@qoncept.co.jp>
2019-06-04 12:16:30 +0900

    Add test

--- Tests/PerformanceTests/ArithmeticPerformanceTests.swift
+++ Tests/PerformanceTests/ArithmeticPerformanceTests.swift
@@ -12,6 +12,18 @@ class ArithmeticPerformanceTests: XCTestCase {
         }
     }
     
+    func testMulScalarEquivalent() {
+        var image = [Double](repeating: 1, count: 3840*2160*4)
+        
+        measure {
+            for _ in 0..<10 {
+                for i in 0..<image.count {
+                    image[i] *= 1.1
+                }
+            }
+        }
+    }
+    
     func testMulPixel() {
         var image = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
         let color = Color<RGBA, Double>(r: 0.9, g: 1.1, b: 0.9, a: 1.1)
@@ -23,6 +35,21 @@ class ArithmeticPerformanceTests: XCTestCase {
         }
     }
     
+    func testMulPixelEquivalent() {
+        var image = [Double](repeating: 1, count: 3840*2160*4)
+        let color = [0.9, 1.1, 0.9, 1.1]
+        
+        measure {
+            for _ in 0..<10 {
+                for i in 0..<image.count/4 {
+                    for j in 0..<4 {
+                        image[4*i+j] *= color[j]
+                    }
+                }
+            }
+        }
+    }
+    
     func testMulImage() {
         var image1 = Image<RGBA, Double>(width: 3840, height: 2160, value: 1)
         let image2 = Image<RGBA, Double>(width: 3840, height: 2160, value: 1.1)
@@ -30,6 +57,19 @@ class ArithmeticPerformanceTests: XCTestCase {
         measure {
             for _ in 0..<10 {
                 image1 *= image2
+            }
+        }
+    }
+    
+    func testMulImageEquivalent() {
+        var image1 = [Double](repeating: 1, count: 3840*2160*4)
+        var image2 = [Double](repeating: 1.1, count: 3840*2160*4)
+        
+        measure {
+            for _ in 0..<10 {
+                for i in 0..<image1.count {
+                    image1[i] *= image2[i]
+                }
             }
         }
     }
