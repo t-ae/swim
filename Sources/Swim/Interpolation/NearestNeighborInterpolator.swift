6c173b7223ba06421f257272a74fb9a649e8d11e
araki <araki@qoncept.co.jp>
2019-06-05 17:46:06 +0900

    Refactor NNInterpolator

--- Sources/Swim/Interpolation/NearestNeighborInterpolator.swift
+++ Sources/Swim/Interpolation/NearestNeighborInterpolator.swift
@@ -11,14 +11,7 @@ public struct NearestNeighborInterpolator<P: PixelType, T: DataType>: Interpolat
     public func interpolate(x: Double, y: Double, in image: Image<P, T>, into pixel: PixelRef<P, T>) {
         let (x, y) = (Int(round(x)), Int(round(y)))
         
-        if let x = edgeMode.clampValue(value: x, max: image.width),
-            let y = edgeMode.clampValue(value: y, max: image.height) {
-            pixel.setColor(x: x, y: y, in: image)
-        } else if case let .constant(px) = edgeMode {
-            pixel.setColor(color: px)
-        } else {
-            fatalError("Never happens")
-        }
+        pixel.setColor(x: x, y: y, in: image, edgeMode: edgeMode)
     }
 }
 
