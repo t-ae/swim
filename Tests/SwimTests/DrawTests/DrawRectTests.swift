1b6562e01c1a4e18821c6cfaf6c0340633f5b345
araki <araki@qoncept.co.jp>
2019-06-05 16:13:28 +0900

    Add test

--- Tests/SwimTests/DrawTests/DrawRectTests.swift
+++ Tests/SwimTests/DrawTests/DrawRectTests.swift
@@ -30,8 +30,41 @@ class DrawRectTests: XCTestCase {
         XCTAssertEqual(image[50..<100, 50..<100], Image.zeros(like: image[0..<50, 0..<50]))
     }
     
+    func testDrawImage() {
+        var image = Image<RGB, Int>(width: 100, height: 100, value: 0)
+        let draw = Image<RGB, Int>(width: 10, height: 10, data: (0..<300).map { Int($0) })
+        
+        image.drawImage(origin: (-5, -5), image: draw)
+        XCTAssertEqual(image[0..<5, 0..<5], draw[5..<10, 5..<10])
+        
+        image.drawImage(origin: (98, 95), image: draw)
+        XCTAssertEqual(image[98..<100, 95..<100], draw[0..<2, 0..<5])
+        
+        var mask = Image<Gray, Bool>.full(value: false, like: draw)
+        mask.drawLine(p1: (1, 1), p2: (1, 3), color: Color(gray: true))
+        mask.drawLine(p1: (1, 1), p2: (5, 1), color: Color(gray: true))
+        image.drawImage(origin: (10, 10), image: draw, mask: mask)
+        XCTAssertEqual(image[11..<16, 11..<12], draw[1..<6, 1..<2])
+        XCTAssertEqual(image[11..<12, 11..<14], draw[1..<2, 1..<4])
+    }
+    
+    func testDrawText() {
+        guard let font = try? TrueTypeFont(url: URL(fileURLWithPath: "/System/Library/Fonts/Helvetica.ttc"), fontSize: 30) else {
+            return
+        }
+        
+        let original = Image<RGB, Int>(width: 100, height: 100, value: 0)
+        var image = original
+        image.drawText(origin: (0, 0), text: "test", font: font, color: .red)
+        
+        // Check at least drawing something
+        XCTAssertNotEqual(image, original)
+    }
+    
     static let allTests = [
         ("testDrawRect", testDrawRect),
         ("testDrawRectPelimeter", testDrawRectPelimeter),
+        ("testDrawImage", testDrawImage),
+        ("testDrawText", testDrawText),
     ]
 }
