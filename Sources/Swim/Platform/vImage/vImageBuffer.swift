ef88850bb4abc55967b78b6fa6a9ba0866f835e0
araki <araki@qoncept.co.jp>
2019-06-14 17:20:06 +0900

    Refactor vImage withBuffer

--- Sources/Swim/Platform/vImage/vImageBuffer.swift
+++ Sources/Swim/Platform/vImage/vImageBuffer.swift
@@ -3,88 +3,30 @@
 import Accelerate
 
 extension vImageUtils {
-    // MARK: UInt8
     @inlinable
-    public static func withBuffer<R>(_ image: inout Image<Gray, UInt8>,
-                                     closure: (inout vImage_Buffer)->R) -> R {
+    public static func withBuffer<P, R>(_ image: inout Image<P, UInt8>,
+                                        closure: (inout vImage_Buffer)->R) -> R {
         let height = image.height
         let width = image.width
         return image.data.withUnsafeMutableBufferPointer {
             var buffer = vImage_Buffer(data: $0.baseAddress!,
                                        height: UInt(height),
                                        width: UInt(width),
-                                       rowBytes: MemoryLayout<UInt8>.size * width * Gray.channels)
+                                       rowBytes: MemoryLayout<UInt8>.size * width * P.channels)
             return closure(&buffer)
         }
     }
     
     @inlinable
-    public static func withBuffer<R>(_ image: inout Image<RGBA, UInt8>,
-                                     closure: (inout vImage_Buffer)->R) -> R {
+    public static func withBuffer<P, R>(_ image: inout Image<P, Float>,
+                                        closure: (inout vImage_Buffer)->R) -> R {
         let height = image.height
         let width = image.width
         return image.data.withUnsafeMutableBufferPointer {
             var buffer = vImage_Buffer(data: $0.baseAddress!,
                                        height: UInt(height),
                                        width: UInt(width),
-                                       rowBytes: MemoryLayout<UInt8>.size * width * RGBA.channels)
-            return closure(&buffer)
-        }
-    }
-    
-    @inlinable
-    public static func withBuffer<R>(_ image: inout Image<ARGB, UInt8>,
-                                     closure: (inout vImage_Buffer)->R) -> R {
-        let height = image.height
-        let width = image.width
-        return image.data.withUnsafeMutableBufferPointer {
-            var buffer = vImage_Buffer(data: $0.baseAddress!,
-                                       height: UInt(height),
-                                       width: UInt(width),
-                                       rowBytes: MemoryLayout<UInt8>.size * width * ARGB.channels)
-            return closure(&buffer)
-        }
-    }
-    
-    // MARK: Float
-    @inlinable
-    public static func withBuffer<R>(_ image: inout Image<Gray, Float>,
-                                     closure: (inout vImage_Buffer)->R) -> R {
-        let height = image.height
-        let width = image.width
-        return image.data.withUnsafeMutableBufferPointer {
-            var buffer = vImage_Buffer(data: $0.baseAddress!,
-                                       height: UInt(height),
-                                       width: UInt(width),
-                                       rowBytes: MemoryLayout<Float>.size * width * Gray.channels)
-            return closure(&buffer)
-        }
-    }
-    
-    @inlinable
-    public static func withBuffer<R>(_ image: inout Image<RGBA, Float>,
-                                     closure: (inout vImage_Buffer)->R) -> R {
-        let height = image.height
-        let width = image.width
-        return image.data.withUnsafeMutableBufferPointer {
-            var buffer = vImage_Buffer(data: $0.baseAddress!,
-                                       height: UInt(height),
-                                       width: UInt(width),
-                                       rowBytes: MemoryLayout<Float>.size * width * RGBA.channels)
-            return closure(&buffer)
-        }
-    }
-    
-    @inlinable
-    public static func withBuffer<R>(_ image: inout Image<ARGB, Float>,
-                                     closure: (inout vImage_Buffer)->R) -> R {
-        let height = image.height
-        let width = image.width
-        return image.data.withUnsafeMutableBufferPointer {
-            var buffer = vImage_Buffer(data: $0.baseAddress!,
-                                       height: UInt(height),
-                                       width: UInt(width),
-                                       rowBytes: MemoryLayout<Float>.size * width * ARGB.channels)
+                                       rowBytes: MemoryLayout<Float>.size * width * P.channels)
             return closure(&buffer)
         }
     }
