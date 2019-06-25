36149a69afc179aa38ca3511884ffe14f601aac5
araki <araki@qoncept.co.jp>
2019-06-25 09:46:48 +0900

    Update ubuntu_test.sh

--- ubuntu_test.sh
+++ ubuntu_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cmd="swift test $1"
+cmd="swift test $@"
 echo "$cmd"
 docker run --rm -w /workspace -v $DIR:/workspace swift:5.0 bash -c "$cmd"
