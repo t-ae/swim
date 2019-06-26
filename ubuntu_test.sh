2911633d003c6b187d40440b9fb3e1d43a879109
araki <araki@qoncept.co.jp>
2019-06-26 17:19:56 +0900

    Remove ubuntu_test

--- ubuntu_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cmd="swift test $@"
-echo "$cmd"
-docker run --rm -w /workspace -v $DIR:/workspace swift:5.0 bash -c "$cmd"
