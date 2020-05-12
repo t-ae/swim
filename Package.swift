2175fd328b022d8d072d64034503a1499a1f39bc
araki <araki@qoncept.co.jp>
2020-05-13 01:10:24 +0900

    Fix maxOS version

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Swim",
     platforms: [
-        .macOS(.v10_12),
+        .macOS(.v10_13),
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
